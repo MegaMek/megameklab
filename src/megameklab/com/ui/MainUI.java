140a43bd13b81119c75f7abfe23773642a051006
torren <torren@users.sourceforge.net>
2008-06-09 15:58:46 +0000

    + fixed bugs in adding and removing TSM crits.

--- src/megameklab/com/ui/MainUI.java
+++ src/megameklab/com/ui/MainUI.java
@@ -56,7 +56,7 @@ public class MainUI extends JFrame implements RefreshListener {
      * 
      */
     private static final long serialVersionUID = -5836932822468918198L;
-    private static final String VERSION = "0.0.0.1";
+    private static final String VERSION = "0.0.0.2";
     Mech entity = null;
     JMenuBar menuBar = new JMenuBar();
     JMenu file = new JMenu("File");
--- src/megameklab/com/ui/util/UnitUtil.java
+++ src/megameklab/com/ui/util/UnitUtil.java
@@ -23,31 +23,35 @@ import megamek.common.Mech;
 import megamek.common.MiscType;
 import megamek.common.Mounted;
 
-
-public class UnitUtil{
+public class UnitUtil {
     /**
      * tells is EquipementType is armor or Structure that uses crits/mounted
+     * 
      * @param eq
      * @return
      */
-    public static boolean isArmorOrStructure(EquipmentType eq){
-        return  eq instanceof MiscType && ( eq.hasFlag(MiscType.F_ENDO_STEEL) || eq.hasFlag(MiscType.F_FERRO_FIBROUS) || eq.hasFlag(MiscType.F_TSM));
-    } 
+    public static boolean isArmorOrStructure(EquipmentType eq) {
+        return eq instanceof MiscType && (eq.hasFlag(MiscType.F_ENDO_STEEL) || eq.hasFlag(MiscType.F_FERRO_FIBROUS) || eq.hasFlag(MiscType.F_TSM));
+    }
+
     /**
      * Returns the number of crits used by EquipmentType eq, 1 if armor or structure EquipmentType
+     * 
      * @param unit
      * @param eq
      * @return
      */
-    public static int getCritsUsed(Mech unit, EquipmentType eq){
-        if ( UnitUtil.isArmorOrStructure(eq) ){
+    public static int getCritsUsed(Mech unit, EquipmentType eq) {
+        if (UnitUtil.isArmorOrStructure(eq)) {
             return 1;
         }
-        
+
         return eq.getCriticals(unit);
     }
+
     /**
      * Removes a Mounted object from the units various equipment lists
+     * 
      * @param unit
      * @param eq
      */
@@ -59,60 +63,116 @@ public class UnitUtil{
                 break;
             }
         }
-        
-        if ( equipment == null ){
+
+        if (equipment == null) {
             return;
         }
-        
+
         UnitUtil.removeCriticals(unit, equipment);
-        
-        unit.getEquipment().remove(equipment);
 
-        if (equipment.getType() instanceof MiscType) {
-            unit.getMisc().remove(equipment);
-        } else if (equipment.getType() instanceof AmmoType) {
-            unit.getAmmo().remove(equipment);
+        if (equipment.getName().equals("TSM")) {
+            UnitUtil.removeTSMMounts(unit);
         } else {
-            unit.getWeaponList().remove(equipment);
+            unit.getEquipment().remove(equipment);
+
+            if (equipment.getType() instanceof MiscType) {
+                unit.getMisc().remove(equipment);
+            } else if (equipment.getType() instanceof AmmoType) {
+                unit.getAmmo().remove(equipment);
+            } else {
+                unit.getWeaponList().remove(equipment);
+            }
         }
 
     }
 
-/**
- * Sets the corresponding critical slots to null for the Mounted object.
- * @param unit
- * @param eq
- */
-    public static void removeCriticals(Mech unit, Mounted eq){
-        int location = eq.getLocation();
-        if (location != Mech.LOC_NONE) {
-            int critsUsed = UnitUtil.getCritsUsed(unit, eq.getType());
-            for (int slot = 0; slot < unit.getNumberOfCriticals(location) && critsUsed > 0; slot++) {
-                CriticalSlot cs = unit.getCritical(location, slot);
-                if (cs != null && cs.getIndex() == unit.getEquipmentNum(eq)) {
-                    cs = null;
-                    unit.setCritical(location, slot, cs);
-                    --critsUsed;
+    /**
+     * Removes TSM Mounts from the Mek.
+     * 
+     * @param Unit
+     */
+    public static void removeTSMMounts(Mech unit) {
+
+        for (int pos = 0; pos < unit.getEquipment().size();) {
+            Mounted mount = unit.getEquipment().get(pos);
+            if (mount.getType().getName().equals("TSM")) {
+                unit.getEquipment().remove(pos);
+                unit.getMisc().remove(mount);
+            } else {
+                pos++;
+            }
+        }
+    }
+
+    /**
+     * Removes all TSM crits from
+     * 
+     * @param unit
+     * @param unit
+     */
+    public static void removeTSMCrits(Mech unit) {
+
+        for (int location = Mech.LOC_HEAD; location <= Mech.LOC_LLEG; location++) {
+            for (int slot = 0; slot < unit.getNumberOfCriticals(location); slot++) {
+                CriticalSlot crit = unit.getCritical(location, slot);
+                if (crit != null && crit.getType() == CriticalSlot.TYPE_EQUIPMENT) {
+                    Mounted mount = unit.getEquipment(crit.getIndex());
+
+                    if (mount != null && mount.getType().getName().equals("TSM")) {
+                        crit = null;
+                        unit.setCritical(location, slot, crit);
+                    }
                 }
             }
         }
     }
-    
-    public static void resetCriticalsAndMounts(Mech unit){
-        for (int location = Mech.LOC_HEAD; location <= Mech.LOC_LLEG; location++ ){
-            for ( int slot = 0; slot < unit.getNumberOfCriticals(location); slot++ ){
+
+    /**
+     * Sets the corresponding critical slots to null for the Mounted object.
+     * 
+     * @param unit
+     * @param eq
+     */
+    public static void removeCriticals(Mech unit, Mounted eq) {
+
+        if (eq.getType().getName().equals("TSM")) {
+            UnitUtil.removeTSMCrits(unit);
+        } else {
+            int location = eq.getLocation();
+            if (location != Mech.LOC_NONE) {
+                int critsUsed = UnitUtil.getCritsUsed(unit, eq.getType());
+                for (int slot = 0; slot < unit.getNumberOfCriticals(location) && critsUsed > 0; slot++) {
+                    CriticalSlot cs = unit.getCritical(location, slot);
+                    if (cs != null && cs.getIndex() == unit.getEquipmentNum(eq)) {
+                        cs = null;
+                        unit.setCritical(location, slot, cs);
+                        --critsUsed;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Reset all the Crits and Mounts on the Unit.
+     * 
+     * @param unit
+     */
+    public static void resetCriticalsAndMounts(Mech unit) {
+        for (int location = Mech.LOC_HEAD; location <= Mech.LOC_LLEG; location++) {
+            for (int slot = 0; slot < unit.getNumberOfCriticals(location); slot++) {
                 CriticalSlot cs = unit.getCritical(location, slot);
-                
-                if ( cs != null && cs.getType() == CriticalSlot.TYPE_EQUIPMENT ){
+
+                if (cs != null && cs.getType() == CriticalSlot.TYPE_EQUIPMENT) {
                     cs = null;
                     unit.setCritical(location, slot, cs);
                 }
             }
         }
-        
-        for ( Mounted mount : unit.getEquipment() ){
-            mount.setLocation(Mech.LOC_NONE,false);
+
+        for (Mounted mount : unit.getEquipment()) {
+            mount.setLocation(Mech.LOC_NONE, false);
         }
-        
+
     }
 }
