69bf4ad7f37c3365dd449b98e89ef67042a90d10
torren <torren@532ea136-1839-46d7-bdbd-97cf88639dd1>
2008-10-01 15:52:39 +0000

    v 0.0.0.3 (10/1/08) + Added Omni Option + Added Quad Option + Converted to new MegaMek Tech Constants

--- trunk/src/megameklab/com/ui/MainUI.java
+++ trunk/src/megameklab/com/ui/MainUI.java
@@ -57,7 +57,7 @@ public class MainUI extends JFrame implements RefreshListener {
      * 
      */
     private static final long serialVersionUID = -5836932822468918198L;
-    private static final String VERSION = "0.0.0.2";
+    private static final String VERSION = "0.0.0.3";
     Mech entity = null;
     JMenuBar menuBar = new JMenuBar();
     JMenu file = new JMenu("File");
