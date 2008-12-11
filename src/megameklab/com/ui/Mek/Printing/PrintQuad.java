7ffebf87428836058a41fee81e3f514e028445b2
torren <torren@users.sourceforge.net>
2008-12-11 04:37:33 +0000

    Quads printing Crits as non bold

--- src/megameklab/com/ui/Mek/Printing/PrintQuad.java
+++ src/megameklab/com/ui/Mek/Printing/PrintQuad.java
@@ -1158,7 +1158,7 @@ public class PrintQuad implements Printable {
                     font = UnitUtil.deriveFont(7.0f);
                     g2d.setFont(font);
                 } else {
-                    g2d.setFont(UnitUtil.getNewFont(g2d, critName.toString(), false, 86, 7.0f));
+                    g2d.setFont(UnitUtil.getNewFont(g2d, critName.toString(), true, 86, 7.0f));
                 }
 
                 if (m.getType() instanceof MiscType && m.getType().hasFlag(MiscType.F_C3I)) {
