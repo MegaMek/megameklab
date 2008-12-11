10ae8f00fbf09bbfe1aec1f21d185f8fa9aca4d0
torren <torren@users.sourceforge.net>
2008-12-11 17:07:21 +0000

    + RFE: Save last known settings in unit dialog

--- src/megameklab/com/ui/Mek/MainUI.java
+++ src/megameklab/com/ui/Mek/MainUI.java
@@ -19,6 +19,7 @@ package megameklab.com.ui.Mek;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FileDialog;
+import java.awt.Frame;
 import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
@@ -42,22 +43,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTabbedPane;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingConstants;
-
-import megameklab.com.ui.dialog.UnitViewerDialog;
-import megameklab.com.ui.Mek.tabs.ArmorTab;
-import megameklab.com.ui.Mek.tabs.BuildTab;
-import megameklab.com.ui.Mek.tabs.EquipmentTab;
-import megameklab.com.ui.Mek.tabs.StructureTab;
-import megameklab.com.ui.Mek.tabs.WeaponTab;
-import megameklab.com.util.CConfig;
-import megameklab.com.ui.Mek.Printing.PrintAdvancedMech;
-import megameklab.com.ui.Mek.Printing.PrintAdvancedQuad;
-import megameklab.com.ui.Mek.Printing.PrintMech;
-import megameklab.com.ui.Mek.Printing.PrintQuad;
-import megameklab.com.util.RefreshListener;
-import megameklab.com.util.SaveMechToMTF;
-import megameklab.com.util.UnitUtil;
 
 import megamek.MegaMek;
 import megamek.client.ui.swing.UnitLoadingDialog;
@@ -69,9 +56,24 @@ import megamek.common.EquipmentType;
 import megamek.common.Mech;
 import megamek.common.QuadMech;
 import megamek.common.TechConstants;
+import megamek.common.UnitType;
 import megamek.common.verifier.EntityVerifier;
 import megamek.common.verifier.TestEntity;
 import megamek.common.verifier.TestMech;
+import megameklab.com.ui.Mek.Printing.PrintAdvancedMech;
+import megameklab.com.ui.Mek.Printing.PrintAdvancedQuad;
+import megameklab.com.ui.Mek.Printing.PrintMech;
+import megameklab.com.ui.Mek.Printing.PrintQuad;
+import megameklab.com.ui.Mek.tabs.ArmorTab;
+import megameklab.com.ui.Mek.tabs.BuildTab;
+import megameklab.com.ui.Mek.tabs.EquipmentTab;
+import megameklab.com.ui.Mek.tabs.StructureTab;
+import megameklab.com.ui.Mek.tabs.WeaponTab;
+import megameklab.com.ui.dialog.UnitViewerDialog;
+import megameklab.com.util.CConfig;
+import megameklab.com.util.RefreshListener;
+import megameklab.com.util.SaveMechToMTF;
+import megameklab.com.util.UnitUtil;
 
 public class MainUI extends JFrame implements RefreshListener {
 
@@ -98,7 +100,7 @@ public class MainUI extends JFrame implements RefreshListener {
     JPanel masterPanel = new JPanel();
     JScrollPane scroll = new JScrollPane();
     public CConfig config;
-    
+
     public MainUI() {
 
         UnitUtil.loadFonts();
@@ -139,10 +141,10 @@ public class MainUI extends JFrame implements RefreshListener {
 
         JMenu printMenu = new JMenu("Print");
         printMenu.setMnemonic('P');
-        
+
         JMenu standardRecordSheet = new JMenu("Standard Record Sheet");
         standardRecordSheet.setMnemonic('S');
-        
+
         item = new JMenuItem("Current Unit");
         item.setMnemonic('C');
         item.addActionListener(new ActionListener() {
@@ -161,10 +163,10 @@ public class MainUI extends JFrame implements RefreshListener {
         });
         standardRecordSheet.add(item);
         printMenu.add(standardRecordSheet);
-        
+
         JMenu advancedRecordSheet = new JMenu("Advanced Record Sheet");
         advancedRecordSheet.setMnemonic('A');
-        
+
         item = new JMenuItem("Current Unit");
         item.setMnemonic('C');
         item.addActionListener(new ActionListener() {
@@ -183,7 +185,7 @@ public class MainUI extends JFrame implements RefreshListener {
         });
         advancedRecordSheet.add(item);
         printMenu.add(advancedRecordSheet);
-        
+
         file.add(printMenu);
 
         file.addSeparator();
@@ -197,7 +199,7 @@ public class MainUI extends JFrame implements RefreshListener {
             }
         });
         file.add(item);
-        
+
         item = new JMenuItem();
         item.setText("About");
         item.setMnemonic('A');
@@ -207,7 +209,7 @@ public class MainUI extends JFrame implements RefreshListener {
             }
         });
         help.add(item);
-        
+
         item = new JMenuItem();
         item.setText("Validate Current Unit");
         item.setMnemonic('V');
@@ -217,7 +219,7 @@ public class MainUI extends JFrame implements RefreshListener {
             }
         });
         validate.add(item);
-        
+
         menuBar.add(file);
         menuBar.add(validate);
         menuBar.add(help);
@@ -227,8 +229,8 @@ public class MainUI extends JFrame implements RefreshListener {
             @Override
             public void windowClosing(WindowEvent evt) {
                 config.setParam("WINDOWSTATE", Integer.toString(getExtendedState()));
-                //Only save position and size if not maximized or minimized.
-                if ( getExtendedState() == JFrame.NORMAL ) {
+                // Only save position and size if not maximized or minimized.
+                if (getExtendedState() == Frame.NORMAL) {
                     config.setParam("WINDOWHEIGHT", Integer.toString(getHeight()));
                     config.setParam("WINDOWWIDTH", Integer.toString(getWidth()));
                     config.setParam("WINDOWLEFT", Integer.toString(getX()));
@@ -242,48 +244,49 @@ public class MainUI extends JFrame implements RefreshListener {
 
         // ConfigPane.setMinimumSize(new Dimension(300, 300));
         createNewMech(false);
-        this.setTitle(entity.getChassis() + " " + entity.getModel() + ".mtf");
+        setTitle(entity.getChassis() + " " + entity.getModel() + ".mtf");
         setJMenuBar(menuBar);
-        scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scroll.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
         scroll.setViewportView(masterPanel);
         scroll.setBorder(BorderFactory.createEmptyBorder());
         this.add(scroll);
         Dimension maxSize = new Dimension(config.getIntParam("WINDOWWIDTH"), config.getIntParam("WINDOWHEIGHT"));
         // masterPanel.setPreferredSize(new Dimension(600,400));
-        //scroll.setPreferredSize(maxSize);
+        // scroll.setPreferredSize(maxSize);
         setResizable(true);
         setSize(maxSize);
         setMaximumSize(maxSize);
         setPreferredSize(maxSize);
         setExtendedState(config.getIntParam("WINDOWSTATE"));
-        setLocation(config.getIntParam("WINDOWLEFT"),config.getIntParam("WINDOWTOP"));
+        setLocation(config.getIntParam("WINDOWLEFT"), config.getIntParam("WINDOWTOP"));
 
         reloadTabs();
-        this.setVisible(true);
+        setVisible(true);
         repaint();
         refreshAll();
     }
 
     private void loadUnit() {
         UnitLoadingDialog unitLoadingDialog = new UnitLoadingDialog(this);
-        UnitViewerDialog viewer = new UnitViewerDialog(this, unitLoadingDialog, 0);
-        
+        UnitViewerDialog viewer = new UnitViewerDialog(this, unitLoadingDialog, UnitType.MEK, config);
+
         viewer.run();
-        //viewer.setVisible(true);
+        // viewer.setVisible(true);
 
         if (viewer != null) {
 
-            if (!(viewer.getSelectedEntity() instanceof Mech))
+            if (!(viewer.getSelectedEntity() instanceof Mech)) {
                 return;
+            }
             entity = (Mech) viewer.getSelectedEntity();
             UnitUtil.expandUnitMounts(entity);
-            
+
             viewer.setVisible(false);
             viewer.dispose();
 
             reloadTabs();
-            this.setVisible(true);
+            setVisible(true);
             this.repaint();
             refreshAll();
         }
@@ -294,7 +297,7 @@ public class MainUI extends JFrame implements RefreshListener {
     }
 
     public void jMenuResetEntity_actionPerformed(ActionEvent event) {
-        
+
         createNewMech(false);
         structureTab.updateMech(entity);
         armorTab.updateMech(entity);
@@ -303,10 +306,10 @@ public class MainUI extends JFrame implements RefreshListener {
         buildTab.updateMech(entity);
         statusbar.updateMech(entity);
         header.updateMech(entity);
-        
+
         weaponTab.getView().removeAllWeapons();
         equipmentTab.getView().removeAllEquipment();
-        
+
         statusbar.refresh();
         structureTab.refresh();
         armorTab.refresh();
@@ -316,20 +319,20 @@ public class MainUI extends JFrame implements RefreshListener {
     }
 
     public void jMenuPrint_actionPerformed(ActionEvent event) {
-        
-        if ( entity instanceof QuadMech ) {
-            ArrayList<Mech>mechList = new ArrayList<Mech>();
+
+        if (entity instanceof QuadMech) {
+            ArrayList<Mech> mechList = new ArrayList<Mech>();
             mechList.add(entity);
 
             PrintQuad sp = new PrintQuad(mechList);
-    
+
             sp.print();
-        }else {
-            ArrayList<Mech>mechList = new ArrayList<Mech>();
+        } else {
+            ArrayList<Mech> mechList = new ArrayList<Mech>();
             mechList.add(entity);
-            
+
             PrintMech sp = new PrintMech(mechList);
-    
+
             sp.print();
         }
     }
@@ -337,7 +340,7 @@ public class MainUI extends JFrame implements RefreshListener {
     public void jMenuPrintMulMechs_actionPerformed(ActionEvent event) {
         ArrayList<Mech> quadList = new ArrayList<Mech>();
         ArrayList<Mech> bipedList = new ArrayList<Mech>();
-        
+
         FileDialog f = new FileDialog(new JDialog(), "Load Mul");
         f.setDirectory(System.getProperty("user.dir"));
         f.setFilenameFilter(new FilenameFilter() {
@@ -346,69 +349,69 @@ public class MainUI extends JFrame implements RefreshListener {
             }
         });
         f.setVisible(true);
-        
-        if ( f.getFile() != null ) {
+
+        if (f.getFile() != null) {
             Vector<Entity> loadedUnits = new Vector<Entity>();
             try {
-                loadedUnits = EntityListFile.loadFrom(new File(f.getDirectory()+f.getFile()));
+                loadedUnits = EntityListFile.loadFrom(new File(f.getDirectory() + f.getFile()));
                 loadedUnits.trimToSize();
             } catch (Exception ex) {
                 ex.printStackTrace();
                 return;
             }
 
-            for ( Entity unit : loadedUnits ) {
-                if ( unit instanceof QuadMech ) {
-                    quadList.add((Mech)unit);
-                }else if ( unit instanceof BipedMech ) {
-                    bipedList.add((Mech)unit);
+            for (Entity unit : loadedUnits) {
+                if (unit instanceof QuadMech) {
+                    quadList.add((Mech) unit);
+                } else if (unit instanceof BipedMech) {
+                    bipedList.add((Mech) unit);
                 }
             }
-            
-            if ( bipedList.size() > 0 ) {
+
+            if (bipedList.size() > 0) {
                 PrintMech printMech = new PrintMech(bipedList);
-        
+
                 printMech.print();
             }
-            
-            if ( quadList.size() > 0 ) {
+
+            if (quadList.size() > 0) {
                 PrintQuad printQuad = new PrintQuad(quadList);
-        
+
                 printQuad.print();
             }
-            
+
         }
-        
+
     }
-    
+
     public void jMenuAdvancedPrint_actionPerformed(ActionEvent event) {
-        
-        if ( entity instanceof QuadMech ) {
+
+        if (entity instanceof QuadMech) {
             String fImageName = "./data/images/recordsheets/toquad.png";
-            
-            ArrayList<Mech>mechList = new ArrayList<Mech>();
+
+            ArrayList<Mech> mechList = new ArrayList<Mech>();
             mechList.add(entity);
 
             PrintAdvancedQuad sp = new PrintAdvancedQuad(getToolkit().getImage(fImageName), mechList);
-   
-            sp.print();
-           
-        }else {
-             String fImageName = "./data/images/recordsheets/tobiped.png";
-    
-             ArrayList<Mech>mechList = new ArrayList<Mech>();
-             mechList.add(entity);
 
-             PrintAdvancedMech sp = new PrintAdvancedMech(getToolkit().getImage(fImageName), mechList);
-    
-             sp.print();
+            sp.print();
+
+        } else {
+            String fImageName = "./data/images/recordsheets/tobiped.png";
+
+            ArrayList<Mech> mechList = new ArrayList<Mech>();
+            mechList.add(entity);
+
+            PrintAdvancedMech sp = new PrintAdvancedMech(getToolkit().getImage(fImageName), mechList);
+
+            sp.print();
         }
-     }
-    
+    }
+
     public void jMenuPrintAdvancedMulMechs_actionPerformed(ActionEvent event) {
         ArrayList<Mech> quadList = new ArrayList<Mech>();
         ArrayList<Mech> bipedList = new ArrayList<Mech>();
-        
+
         FileDialog f = new FileDialog(new JDialog(), "Load Mul");
         f.setDirectory(System.getProperty("user.dir"));
         f.setFilenameFilter(new FilenameFilter() {
@@ -417,49 +420,48 @@ public class MainUI extends JFrame implements RefreshListener {
             }
         });
         f.setVisible(true);
-        
-        if ( f.getFile() != null ) {
+
+        if (f.getFile() != null) {
             Vector<Entity> loadedUnits = new Vector<Entity>();
             try {
-                loadedUnits = EntityListFile.loadFrom(new File(f.getDirectory()+f.getFile()));
+                loadedUnits = EntityListFile.loadFrom(new File(f.getDirectory() + f.getFile()));
                 loadedUnits.trimToSize();
             } catch (Exception ex) {
                 ex.printStackTrace();
                 return;
             }
 
-            for ( Entity unit : loadedUnits ) {
-                if ( unit instanceof QuadMech ) {
-                    quadList.add((Mech)unit);
-                }else if ( unit instanceof BipedMech ) {
-                    bipedList.add((Mech)unit);
+            for (Entity unit : loadedUnits) {
+                if (unit instanceof QuadMech) {
+                    quadList.add((Mech) unit);
+                } else if (unit instanceof BipedMech) {
+                    bipedList.add((Mech) unit);
                 }
             }
-            
+
             String fImageName = "./data/images/recordsheets/tobiped.png";
-            
+
             Image image = getToolkit().getImage(fImageName);
-            
-            if ( bipedList.size() > 0 ) {
+
+            if (bipedList.size() > 0) {
                 PrintAdvancedMech printMech = new PrintAdvancedMech(image, bipedList);
-        
+
                 printMech.print();
             }
-            
+
             fImageName = "./data/images/recordsheets/toquad.png";
-            
+
             image = getToolkit().getImage(fImageName);
 
-            if ( quadList.size() > 0 ) {
+            if (quadList.size() > 0) {
                 PrintAdvancedQuad printQuad = new PrintAdvancedQuad(image, quadList);
-        
+
                 printQuad.print();
             }
-            
+
         }
-        
+
     }
-    
 
     // Show Validation data.
     public void jMenuValidateUnit_actionPerformed() {
@@ -467,19 +469,19 @@ public class MainUI extends JFrame implements RefreshListener {
         EntityVerifier entityVerifier = new EntityVerifier(new File("data/mechfiles/UnitVerifierOptions.xml"));
         StringBuffer sb = new StringBuffer();
         TestEntity testEntity = null;
-        testEntity = new TestMech((Mech) entity, entityVerifier.mechOption, null);
+        testEntity = new TestMech(entity, entityVerifier.mechOption, null);
         if (testEntity != null) {
             testEntity.correctEntity(sb, true);
         }
 
-        if ( sb.length() > 0 ){
-            JOptionPane.showMessageDialog(this, sb.toString(),"Unit Validation",JOptionPane.ERROR_MESSAGE);
-        } else{
-            JOptionPane.showMessageDialog(this, "Validation Passed","Unit Validation",JOptionPane.INFORMATION_MESSAGE);
+        if (sb.length() > 0) {
+            JOptionPane.showMessageDialog(this, sb.toString(), "Unit Validation", JOptionPane.ERROR_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(this, "Validation Passed", "Unit Validation", JOptionPane.INFORMATION_MESSAGE);
         }
-            
+
     }
-    
+
     // Show data about MegaMekLab
     public void jMenuHelpAbout_actionPerformed() {
 
@@ -543,7 +545,7 @@ public class MainUI extends JFrame implements RefreshListener {
         armorTab.refresh();
 
         structureTab = new StructureTab(entity);
-        
+
         header = new Header(entity);
         statusbar = new StatusBar(entity);
         equipmentTab = new EquipmentTab(entity);
@@ -639,7 +641,7 @@ public class MainUI extends JFrame implements RefreshListener {
     }
 
     public void refreshHeader() {
-        this.setTitle(entity.getChassis() + " " + entity.getModel() + ".mtf");
+        setTitle(entity.getChassis() + " " + entity.getModel() + ".mtf");
     }
 
     public void refreshStatus() {
