aa6a314cfd3b9f9adb41782f62f7b3071d866d02
beerockxs <sebastian@brocks.cc>
2008-12-22 23:57:23 +0000

    tabs bad!

--- src/megameklab/com/util/SpringLayoutHelper.java
+++ src/megameklab/com/util/SpringLayoutHelper.java
@@ -30,9 +30,9 @@ import javax.swing.SpringLayout;
  * @urgru
  */
 public class SpringLayoutHelper {
-	
-	public static SpringLayoutHelper slh;
-	
+    
+    public static SpringLayoutHelper slh;
+    
     public static void setupSpringGrid(JPanel panel, int columns) {
         int count = panel.getComponentCount();
         
@@ -41,51 +41,51 @@ public class SpringLayoutHelper {
         setupSpringGrid(panel,rows,columns);
     }
 
-	public static void setupSpringGrid(JPanel panel,int rows, int columns) {
-		
-		//setup new layout.
-		SpringLayout layout = (SpringLayout)panel.getLayout();
-		
+    public static void setupSpringGrid(JPanel panel,int rows, int columns) {
+        
+        //setup new layout.
+        SpringLayout layout = (SpringLayout)panel.getLayout();
+        
         //add padding so that the count matches
         if ( panel.getComponentCount() < (rows*columns) ){
             for ( int x = panel.getComponentCount(); x < (rows*columns); x++ )
                 panel.add(new JLabel(" "));
         }
         
-		//make all cells in each row same height.
-		Spring y = Spring.constant(4);
-		for (int r = 0; r < rows; r++) {
-			Spring height = Spring.constant(0);
-			for (int c = 0; c < columns; c++) {
-				height = Spring.max(height,layout.getConstraints(panel.getComponent(r*columns + c)).getHeight());
-			}
-			for (int c = 0; c < columns; c++) {
-				SpringLayout.Constraints constraints = layout.getConstraints(panel.getComponent(r*columns + c));
-				constraints.setY(y);
-				constraints.setHeight(height);
-			}
-			y = Spring.sum(y, Spring.sum(height, Spring.constant(4)));
-		}
-		
-		//make all cells in each column the same width.
-		Spring x = Spring.constant(4);
-		for (int c = 0; c < columns; c++) {
-			Spring width = Spring.constant(0);
-			for (int r = 0; r < rows; r++) {
-				width = Spring.max(width,layout.getConstraints(panel.getComponent(r*columns + c)).getWidth());
-			}
-			for (int r = 0; r < rows; r++) {
-				SpringLayout.Constraints constraints = layout.getConstraints(panel.getComponent(r*columns + c));
-				constraints.setX(x);
-				constraints.setWidth(width);
-			}
-			x = Spring.sum(x, Spring.sum(width, Spring.constant(4)));
-		}
-		
-		//Set the parent's size.
-		SpringLayout.Constraints panelConstraints = layout.getConstraints(panel);
-		panelConstraints.setConstraint(SpringLayout.SOUTH, y);
-		panelConstraints.setConstraint(SpringLayout.EAST, x);
-		
-	}//end setupGrid
+        //make all cells in each row same height.
+        Spring y = Spring.constant(4);
+        for (int r = 0; r < rows; r++) {
+            Spring height = Spring.constant(0);
+            for (int c = 0; c < columns; c++) {
+                height = Spring.max(height,layout.getConstraints(panel.getComponent(r*columns + c)).getHeight());
+            }
+            for (int c = 0; c < columns; c++) {
+                SpringLayout.Constraints constraints = layout.getConstraints(panel.getComponent(r*columns + c));
+                constraints.setY(y);
+                constraints.setHeight(height);
+            }
+            y = Spring.sum(y, Spring.sum(height, Spring.constant(4)));
+        }
+        
+        //make all cells in each column the same width.
+        Spring x = Spring.constant(4);
+        for (int c = 0; c < columns; c++) {
+            Spring width = Spring.constant(0);
+            for (int r = 0; r < rows; r++) {
+                width = Spring.max(width,layout.getConstraints(panel.getComponent(r*columns + c)).getWidth());
+            }
+            for (int r = 0; r < rows; r++) {
+                SpringLayout.Constraints constraints = layout.getConstraints(panel.getComponent(r*columns + c));
+                constraints.setX(x);
+                constraints.setWidth(width);
+            }
+            x = Spring.sum(x, Spring.sum(width, Spring.constant(4)));
+        }
+        
+        //Set the parent's size.
+        SpringLayout.Constraints panelConstraints = layout.getConstraints(panel);
+        panelConstraints.setConstraint(SpringLayout.SOUTH, y);
+        panelConstraints.setConstraint(SpringLayout.EAST, x);
+        
+    }//end setupGrid
 }//end SpringLayoutHelper
