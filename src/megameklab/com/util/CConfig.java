50a73e8defaf7f2e357f434cf44ccd009f49c083
beerockxs <sebastian@brocks.cc>
2008-12-22 23:57:23 +0000

    tabs bad!

--- src/megameklab/com/util/CConfig.java
+++ src/megameklab/com/util/CConfig.java
@@ -28,25 +28,25 @@ import java.util.Properties;
  * Class for Client's configuration.
  */
 public class CConfig {
-	
-	//VARIABLES
-	public static final String CONFIG_FILE = "./data/config.txt";
+    
+    //VARIABLES
+    public static final String CONFIG_FILE = "./data/config.txt";
     public static final String CONFIG_BACKUP_FILE = "./data/config.txt.bak";
-	
-	private Properties config;//config. player values.
-	
-	//CONSTRUCTOR
-	public CConfig() {
-		
-		config = setDefaults();		
-		//check to see if a config is present. if not, make one.
+    
+    private Properties config;//config. player values.
+    
+    //CONSTRUCTOR
+    public CConfig() {
+        
+        config = setDefaults();        
+        //check to see if a config is present. if not, make one.
         if ( !(new File(CONFIG_FILE).exists()) && !(new File(CONFIG_BACKUP_FILE).exists()) )
-			createConfig();
-		
-		//load the saved config.txt file 
-		try {
-			File configfile = new File(CONFIG_FILE);
-			FileInputStream fis = new FileInputStream(configfile);
+            createConfig();
+        
+        //load the saved config.txt file 
+        try {
+            File configfile = new File(CONFIG_FILE);
+            FileInputStream fis = new FileInputStream(configfile);
             File backupfile = new File(CONFIG_BACKUP_FILE);
             if ( backupfile.exists() ){
                 FileInputStream backupStream = new FileInputStream(backupfile);
@@ -63,7 +63,7 @@ public class CConfig {
             }else
                 config.load(fis);
             fis.close();
-		} catch (IOException ie){
+        } catch (IOException ie){
             try {
                 File configfile = new File(CONFIG_BACKUP_FILE);
                 FileInputStream fis = new FileInputStream(configfile);
@@ -74,34 +74,34 @@ public class CConfig {
             }
         } catch (Exception ex) {
             ex.printStackTrace();
-		}
-		
-		//check for a serverdata.dat
-		try {
-			File configfile = new File("serverdata.dat");
-			FileInputStream fis = new FileInputStream(configfile);
-			config.load(fis);
-			fis.close();
-			configfile.delete();
-			this.saveConfig();
-		} catch (FileNotFoundException fnfe) {
-			//Exception simply means serverdata.dat is not present.
-		}catch ( Exception ex){
-		    ex.printStackTrace();
-		    
-		}
-	}
-	
-	//METHODS
-	/**
-	 * Private method that loads hardcoded defaults. These are loaded
-	 * before the players config values, adding any new configs in their
-	 * default position and ensuring that no config value is even missing.
-	 */
-	private Properties setDefaults() {
-		Properties defaults = new Properties();
-		
-		//Window Locations
+        }
+        
+        //check for a serverdata.dat
+        try {
+            File configfile = new File("serverdata.dat");
+            FileInputStream fis = new FileInputStream(configfile);
+            config.load(fis);
+            fis.close();
+            configfile.delete();
+            this.saveConfig();
+        } catch (FileNotFoundException fnfe) {
+            //Exception simply means serverdata.dat is not present.
+        }catch ( Exception ex){
+            ex.printStackTrace();
+            
+        }
+    }
+    
+    //METHODS
+    /**
+     * Private method that loads hardcoded defaults. These are loaded
+     * before the players config values, adding any new configs in their
+     * default position and ensuring that no config value is even missing.
+     */
+    private Properties setDefaults() {
+        Properties defaults = new Properties();
+        
+        //Window Locations
         defaults.setProperty("WINDOWSTATE", "0");
         defaults.setProperty("WINDOWHEIGHT", "600");
         defaults.setProperty("WINDOWWIDTH", "800");
@@ -110,73 +110,73 @@ public class CConfig {
         
         
         return defaults;
-	}
-	
-	//Creates a new config file
-	public void createConfig() {
-		try {
-			FileOutputStream fos = new FileOutputStream(CONFIG_FILE);
-			PrintStream ps = new PrintStream(fos);
+    }
+    
+    //Creates a new config file
+    public void createConfig() {
+        try {
+            FileOutputStream fos = new FileOutputStream(CONFIG_FILE);
+            PrintStream ps = new PrintStream(fos);
 
-			ps.close();
-			fos.close();
-		} catch (Exception ex) {
-			System.exit(0);
-		}
-	}
-	
-	/**
-	 * Get a config value.
-	 */
-	public String getParam(String param) {
-		String tparam = null;
-		
-		if (param.endsWith(":")) {
-			param = param.substring(0, param.lastIndexOf(":"));
-		}
-		tparam = config.getProperty(param);
-		if (tparam == null) {
-			tparam = "";
-		}
-		return tparam;
-	}	
-	
-	/**
-	 * Set a config value.
-	 */
-	public void setParam(String param, String value) {
-		config.setProperty(param, value);
-	}
-	
-	/**
-	 * See if a paramater is enabled (YES, TRUE or ON).
-	 */
-	public boolean isParam(String param) {
-		String tparam = getParam(param);
-		if (tparam.equalsIgnoreCase("YES") || tparam.equalsIgnoreCase("TRUE") || tparam.equalsIgnoreCase("ON"))
-			return true;
-		return false;
-	}
-	
-	/**
-	 * Return the int value of a given config property. Return
-	 * a 0 if the property is a non-number. Used mostly by the
-	 * misc. mail tab checks.
-	 */
-	public int getIntParam(String param) {
-		int toReturn;
-		try {
-			toReturn = Integer.parseInt(getParam(param));
-		} catch (Exception ex) {
-			return 0;
-		}
-		return toReturn;
-	}
-		
-	/**
-	 * Write the config file out to ./data/mwconfig.txt.
-	 */
-	public void saveConfig() {
+            ps.close();
+            fos.close();
+        } catch (Exception ex) {
+            System.exit(0);
+        }
+    }
+    
+    /**
+     * Get a config value.
+     */
+    public String getParam(String param) {
+        String tparam = null;
+        
+        if (param.endsWith(":")) {
+            param = param.substring(0, param.lastIndexOf(":"));
+        }
+        tparam = config.getProperty(param);
+        if (tparam == null) {
+            tparam = "";
+        }
+        return tparam;
+    }    
+    
+    /**
+     * Set a config value.
+     */
+    public void setParam(String param, String value) {
+        config.setProperty(param, value);
+    }
+    
+    /**
+     * See if a paramater is enabled (YES, TRUE or ON).
+     */
+    public boolean isParam(String param) {
+        String tparam = getParam(param);
+        if (tparam.equalsIgnoreCase("YES") || tparam.equalsIgnoreCase("TRUE") || tparam.equalsIgnoreCase("ON"))
+            return true;
+        return false;
+    }
+    
+    /**
+     * Return the int value of a given config property. Return
+     * a 0 if the property is a non-number. Used mostly by the
+     * misc. mail tab checks.
+     */
+    public int getIntParam(String param) {
+        int toReturn;
+        try {
+            toReturn = Integer.parseInt(getParam(param));
+        } catch (Exception ex) {
+            return 0;
+        }
+        return toReturn;
+    }
+        
+    /**
+     * Write the config file out to ./data/mwconfig.txt.
+     */
+    public void saveConfig() {
 
         try {
             
@@ -192,16 +192,16 @@ public class CConfig {
             return;
         }
         try {
-			FileOutputStream fos = new FileOutputStream(CONFIG_FILE);
-			PrintStream ps = new PrintStream(fos);
-			config.store(ps,"Client Config");
-			fos.close();
-			ps.close();
-		}catch (FileNotFoundException fnfe){
+            FileOutputStream fos = new FileOutputStream(CONFIG_FILE);
+            PrintStream ps = new PrintStream(fos);
+            config.store(ps,"Client Config");
+            fos.close();
+            ps.close();
+        }catch (FileNotFoundException fnfe){
             
         } catch (Exception ex) {
-		    ex.printStackTrace();
-		}
-	}
-	
+            ex.printStackTrace();
+        }
+    }
+    
 }
