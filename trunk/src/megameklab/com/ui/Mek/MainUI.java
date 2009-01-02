0641be5c8083ea1eafad7541e7709e99b8e13d11
torren <torren@532ea136-1839-46d7-bdbd-97cf88639dd1>
2009-01-02 22:40:48 +0000

    + New Mech Templates

--- trunk/src/megameklab/com/ui/Mek/MainUI.java
+++ trunk/src/megameklab/com/ui/Mek/MainUI.java
@@ -85,7 +85,7 @@ public class MainUI extends JFrame implements RefreshListener {
      *
      */
 	private static final long serialVersionUID = -5836932822468918198L;
-	private static final String VERSION = "0.0.0.10-104";
+	private static final String VERSION = "0.0.0.10-106";
 
 	Mech entity = null;
 	JMenuBar menuBar = new JMenuBar();
