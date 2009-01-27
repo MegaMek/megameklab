481184fdc661b8ef76a5f523a87ad5b0690c7b90
torren <torren@users.sourceforge.net>
2009-01-27 23:13:26 +0000

    + Bug: Enviromental Sealing weighing too much

--- src/megameklab/com/ui/Mek/MainUI.java
+++ src/megameklab/com/ui/Mek/MainUI.java
@@ -89,7 +89,7 @@ public class MainUI extends JFrame implements RefreshListener {
      *
      */
 	private static final long serialVersionUID = -5836932822468918198L;
-	private static final String VERSION = "0.0.0.10-116";
+	private static final String VERSION = "0.0.0.10-117";
 
 	Mech entity = null;
 	JMenuBar menuBar = new JMenuBar();
