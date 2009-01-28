a2aacb4e8b4b4880f935e5b213a2c2247f7759f1
beerockxs <beerockxs@532ea136-1839-46d7-bdbd-97cf88639dd1>
2009-01-28 18:10:31 +0000

    + Moved IS to structure tab + fixed a bug where changing armor type would remove equipment such as void-sig,     null-sig, environmental sealing

--- trunk/src/megameklab/com/util/SpringLayoutHelper.java
+++ trunk/src/megameklab/com/util/SpringLayoutHelper.java
@@ -1,6 +1,6 @@
 /*
- * MegaMekLab - Copyright (C) 2008 
- * 
+ * MegaMekLab - Copyright (C) 2008
+ *
  * Original author - jtighe (torren@users.sourceforge.net)
  *
  * This program is free software; you can redistribute it and/or modify it
@@ -22,36 +22,37 @@ import javax.swing.JPanel;
 import javax.swing.Spring;
 import javax.swing.SpringLayout;
 
-/*
+/**
  * Helper which loops through container components in order
  * to set up springs. Uses fixed buffers of 4 pixels between
  * components and frame border.
- * 
+ *
  * @urgru
  */
 public class SpringLayoutHelper {
-    
+
     public static SpringLayoutHelper slh;
-    
+
     public static void setupSpringGrid(JPanel panel, int columns) {
         int count = panel.getComponentCount();
-        
+
         int rows = (int)Math.ceil((double)count/(double)columns);
-        
+
         setupSpringGrid(panel,rows,columns);
     }
 
     public static void setupSpringGrid(JPanel panel,int rows, int columns) {
-        
+
         //setup new layout.
         SpringLayout layout = (SpringLayout)panel.getLayout();
-        
+
         //add padding so that the count matches
         if ( panel.getComponentCount() < (rows*columns) ){
-            for ( int x = panel.getComponentCount(); x < (rows*columns); x++ )
+            for ( int x = panel.getComponentCount(); x < (rows*columns); x++ ) {
                 panel.add(new JLabel(" "));
+            }
         }
-        
+
         //make all cells in each row same height.
         Spring y = Spring.constant(4);
         for (int r = 0; r < rows; r++) {
@@ -66,7 +67,7 @@ public class SpringLayoutHelper {
             }
             y = Spring.sum(y, Spring.sum(height, Spring.constant(4)));
         }
-        
+
         //make all cells in each column the same width.
         Spring x = Spring.constant(4);
         for (int c = 0; c < columns; c++) {
@@ -81,11 +82,11 @@ public class SpringLayoutHelper {
             }
             x = Spring.sum(x, Spring.sum(width, Spring.constant(4)));
         }
-        
+
         //Set the parent's size.
         SpringLayout.Constraints panelConstraints = layout.getConstraints(panel);
         panelConstraints.setConstraint(SpringLayout.SOUTH, y);
         panelConstraints.setConstraint(SpringLayout.EAST, x);
-        
+
     }//end setupGrid
 }//end SpringLayoutHelper
