cdf55edef3240dcfa569013392ccaa37a864ec2f
torren <torren@users.sourceforge.net>
2009-02-02 21:05:32 +0000

    + RFE: Added (Clan)/(IS) tags to weapons and equipment when using MixTech Units

--- src/megameklab/com/ui/Mek/MainUI.java
+++ src/megameklab/com/ui/Mek/MainUI.java
@@ -92,7 +92,7 @@ public class MainUI extends JFrame implements RefreshListener {
      *
      */
     private static final long serialVersionUID = -5836932822468918198L;
-    private static final String VERSION = "0.0.0.11-Dev-102";
+    private static final String VERSION = "0.0.0.11-Dev-103";
 
     Mech entity = null;
     JMenuBar menuBar = new JMenuBar();
