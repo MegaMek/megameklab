72b1aa9582d439b6a42930a0445ef87688622413
torren <torren@users.sourceforge.net>
2009-02-06 20:38:05 +0000

    + Adjusted Composite String on Biped RS + New MegaMek.jar

--- src/megameklab/com/ui/Mek/MainUI.java
+++ src/megameklab/com/ui/Mek/MainUI.java
@@ -93,7 +93,7 @@ public class MainUI extends JFrame implements RefreshListener {
      *
      */
     private static final long serialVersionUID = -5836932822468918198L;
-    private static final String VERSION = "0.0.0.12-DEV-103";
+    private static final String VERSION = "0.0.0.12-DEV-104";
 
     Mech entity = null;
     JMenuBar menuBar = new JMenuBar();
