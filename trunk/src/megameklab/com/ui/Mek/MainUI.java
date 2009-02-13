e609cd0db8747ec9dc4dbb8955dabf43c112b153
torren <torren@532ea136-1839-46d7-bdbd-97cf88639dd1>
2009-02-13 19:17:28 +0000

    + RFE: Add Source Field + RFE: Added Source Field to Print outs next to Era + New MegaMek.jar

--- trunk/src/megameklab/com/ui/Mek/MainUI.java
+++ trunk/src/megameklab/com/ui/Mek/MainUI.java
@@ -94,7 +94,7 @@ public class MainUI extends JFrame implements RefreshListener {
      *
      */
     private static final long serialVersionUID = -5836932822468918198L;
-    private static final String VERSION = "0.0.0.12-DEV-106";
+    private static final String VERSION = "0.0.0.12-DEV-107";
 
     Mech entity = null;
     JMenuBar menuBar = new JMenuBar();
