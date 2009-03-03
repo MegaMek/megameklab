ba55c7831757f121f49ec0d92aefddbfec31e77c
torren <torren@users.sourceforge.net>
2009-03-03 18:39:04 +0000

    V 0.0.0.13 (03/03/2009 10:36 UTC-8)

--- src/megameklab/com/ui/Mek/MainUI.java
+++ src/megameklab/com/ui/Mek/MainUI.java
@@ -97,7 +97,7 @@ public class MainUI extends JFrame implements RefreshListener {
      *
      */
     private static final long serialVersionUID = -5836932822468918198L;
-    private static final String VERSION = "0.0.0.13-104";
+    private static final String VERSION = "0.0.0.13";
 
     Mech entity = null;
     JMenuBar menuBar = new JMenuBar();
