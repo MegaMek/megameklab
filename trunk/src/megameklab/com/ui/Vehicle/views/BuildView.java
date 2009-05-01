0e123d5d43b7c8312756591af6d65b6cd54fa898
Torren <Torren@532ea136-1839-46d7-bdbd-97cf88639dd1>
2009-05-01 05:00:13 +0000

    + Updated CASE text for Clan Quads + added shield image for Biped Mechs + added code to print out shield pips + Changed shield pips from polygons to unicode characters + Updated shield pips + added new right and left shield images + added Era field to Aero RS + RFE: Add context menu on build tab build table to add component to unit + RFE: Set Split Ratio for Splitable equipment + Bug: Claws showing as Tonnage/5 instead of Tonnage/7 for damage. + Added Tank UI + Added splash screens + RFE 2780128: Add delete to right click menu on build tab + Added new diamond image for Shield. + Updated code to use new MegaMek Equipment Flags + New MegaMek.jar

---
+++ trunk/src/megameklab/com/ui/Vehicle/views/BuildView.java
@@ -0,0 +1,291 @@
+/*
+ * MegaMekLab - Copyright (C) 2009
+ *
+ * Original author - jtighe (torren@users.sourceforge.net)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+package megameklab.com.ui.Vehicle.views;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Collections;
+import java.util.Vector;
+
+import javax.swing.BoxLayout;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+
+import megamek.common.AmmoType;
+import megamek.common.Entity;
+import megamek.common.EquipmentType;
+import megamek.common.MiscType;
+import megamek.common.Mounted;
+import megamek.common.Tank;
+import megamek.common.weapons.Weapon;
+import megameklab.com.ui.Vehicle.tabs.BuildTab;
+import megameklab.com.util.CriticalTableModel;
+import megameklab.com.util.CriticalTransferHandler;
+import megameklab.com.util.IView;
+import megameklab.com.util.RefreshListener;
+import megameklab.com.util.StringUtils;
+import megameklab.com.util.UnitUtil;
+
+public class BuildView extends IView implements ActionListener, MouseListener {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 799195356642563937L;
+
+    private JPanel mainPanel = new JPanel();
+
+    private CriticalTableModel equipmentList;
+    private Vector<EquipmentType> masterEquipmentList = new Vector<EquipmentType>(10, 1);
+    private JTable equipmentTable = new JTable();
+    private JScrollPane equipmentScroll = new JScrollPane();
+
+    public BuildView(Tank unit) {
+        super(unit);
+
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+        equipmentList = new CriticalTableModel(this.unit, CriticalTableModel.BUILDTABLE);
+
+        equipmentTable.setModel(equipmentList);
+        equipmentTable.setDragEnabled(true);
+        CriticalTransferHandler cth = new CriticalTransferHandler(unit, null);
+        equipmentTable.setTransferHandler(cth);
+
+        equipmentList.initColumnSizes(equipmentTable);
+
+        for (int i = 0; i < equipmentList.getColumnCount(); i++) {
+            equipmentTable.getColumnModel().getColumn(i).setCellRenderer(equipmentList.getRenderer());
+        }
+
+        equipmentTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        // equipmentScroll.setToolTipText("");
+        equipmentScroll.setPreferredSize(new Dimension(getWidth(), getHeight()));
+        equipmentTable.setDoubleBuffered(true);
+        equipmentScroll.setViewportView(equipmentTable);
+
+        mainPanel.add(equipmentScroll);
+        equipmentTable.addMouseListener(this);
+
+        this.add(mainPanel);
+        // loadEquipmentTable();
+
+    }
+
+    public void addRefreshedListener(RefreshListener l) {
+    }
+
+    private void loadEquipmentTable() {
+        equipmentList.removeAllCrits();
+        masterEquipmentList.clear();
+        for (Mounted mount : unit.getMisc()) {
+            if (mount.getLocation() == Entity.LOC_NONE) {
+                masterEquipmentList.add(mount.getType());
+            }
+        }
+        for (Mounted mount : unit.getWeaponList()) {
+            if (mount.getLocation() == Entity.LOC_NONE) {
+                masterEquipmentList.add(mount.getType());
+            }
+        }
+        for (Mounted mount : unit.getAmmo()) {
+            if (mount.getLocation() == Entity.LOC_NONE) {
+                masterEquipmentList.add(mount.getType());
+            }
+        }
+
+        Collections.sort(masterEquipmentList, StringUtils.equipmentTypeComparator());
+
+        // Time to Sort
+        // HeatSinks first
+        for (int pos = 0; pos < masterEquipmentList.size(); pos++) {
+            if (UnitUtil.isHeatSink(masterEquipmentList.get(pos))) {
+                equipmentList.addCrit(masterEquipmentList.get(pos));
+                masterEquipmentList.remove(pos);
+                pos--;
+            }
+        }
+
+        // Jump Jets
+        for (int pos = 0; pos < masterEquipmentList.size(); pos++) {
+            if (UnitUtil.isJumpJet(masterEquipmentList.get(pos))) {
+                equipmentList.addCrit(masterEquipmentList.get(pos));
+                masterEquipmentList.remove(pos);
+                pos--;
+            }
+        }
+
+        // weapons and ammo
+        Vector<EquipmentType> weaponsNAmmoList = new Vector<EquipmentType>(10, 1);
+        for (int pos = 0; pos < masterEquipmentList.size(); pos++) {
+            if (masterEquipmentList.get(pos) instanceof Weapon || masterEquipmentList.get(pos) instanceof AmmoType) {
+                weaponsNAmmoList.add(masterEquipmentList.get(pos));
+                masterEquipmentList.remove(pos);
+                pos--;
+            }
+        }
+        Collections.sort(weaponsNAmmoList, StringUtils.equipmentTypeComparator());
+        for (EquipmentType eq : weaponsNAmmoList) {
+            equipmentList.addCrit(eq);
+        }
+
+        // Equipment
+        for (int pos = 0; pos < masterEquipmentList.size(); pos++) {
+            if (masterEquipmentList.get(pos) instanceof MiscType && UnitUtil.isArmor(masterEquipmentList.get(pos)) && UnitUtil.isTSM(masterEquipmentList.get(pos))) {
+                equipmentList.addCrit(masterEquipmentList.get(pos));
+                masterEquipmentList.remove(pos);
+                pos--;
+            }
+        }
+
+        // structure
+        for (int pos = 0; pos < masterEquipmentList.size(); pos++) {
+            if (masterEquipmentList.get(pos) instanceof MiscType && masterEquipmentList.get(pos).hasFlag(MiscType.F_ENDO_STEEL)) {
+                equipmentList.addCrit(masterEquipmentList.get(pos));
+                masterEquipmentList.remove(pos);
+                pos--;
+            }
+        }
+
+        // armor
+        for (int pos = 0; pos < masterEquipmentList.size(); pos++) {
+            if (UnitUtil.isArmor(masterEquipmentList.get(pos))) {
+                equipmentList.addCrit(masterEquipmentList.get(pos));
+                masterEquipmentList.remove(pos);
+                pos--;
+            }
+        }
+
+        // everything else that is not TSM
+        for (int pos = 0; pos < masterEquipmentList.size(); pos++) {
+            if (!UnitUtil.isTSM(masterEquipmentList.get(pos))) {
+                equipmentList.addCrit(masterEquipmentList.get(pos));
+                masterEquipmentList.remove(pos);
+                pos--;
+            }
+        }
+
+        // TSM
+        for (int pos = 0; pos < masterEquipmentList.size(); pos++) {
+            equipmentList.addCrit(masterEquipmentList.get(pos));
+        }
+
+    }
+
+    public void refresh() {
+        removeAllListeners();
+        loadEquipmentTable();
+        fireTableRefresh();
+        addAllListeners();
+    }
+
+    private void removeAllListeners() {
+    }
+
+    private void addAllListeners() {
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        fireTableRefresh();
+    }
+
+    private void fireTableRefresh() {
+        equipmentList.updateUnit(unit);
+        equipmentList.refreshModel();
+        equipmentScroll.setPreferredSize(new Dimension(getWidth() * 90 / 100, getHeight() * 90 / 100));
+        equipmentScroll.repaint();
+    }
+
+    public CriticalTableModel getTableModel() {
+        return equipmentList;
+    }
+
+    public JTable getTable() {
+        return equipmentTable;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON3) {
+            JPopupMenu popup = new JPopupMenu();
+            JMenuItem item;
+
+            final int selectedRow = equipmentTable.rowAtPoint(e.getPoint());
+            String[] locations;
+
+            locations = Tank.LOCATION_NAMES;
+
+            for (int location = 0; location < getTank().locations(); location++) {
+
+                item = new JMenuItem("Add to " + locations[location]);
+
+                final int loc = location;
+                item.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        jMenuLoadComponent_actionPerformed(loc, selectedRow);
+                    }
+                });
+                popup.add(item);
+            }
+
+            popup.show(this, e.getX(), e.getY());
+        }
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    private void jMenuLoadComponent_actionPerformed(int location, int selectedRow) {
+        Mounted eq = UnitUtil.getMounted(unit, ((EquipmentType) equipmentTable.getModel().getValueAt(selectedRow, CriticalTableModel.EQUIPMENT)).getInternalName());
+        try {
+            unit.addEquipment(eq.getType(), location, false);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        UnitUtil.changeMountStatus(unit, eq, location, -1, false);
+
+        // go back up to grandparent build tab and fire a full refresh.
+        ((BuildTab) getParent().getParent()).refreshAll();
+    }
+}
