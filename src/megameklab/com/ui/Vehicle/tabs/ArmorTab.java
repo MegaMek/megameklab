60bf540be97c0fbab4971cbc4a6d704a0db9cd9a
torren <torren@users.sourceforge.net>
2009-05-03 03:11:44 +0000

    + Updated CASE text for Clan Quads + added shield image for Biped Mechs + added code to print out shield pips + Changed shield pips from polygons to unicode characters + Updated shield pips + added new right and left shield images + added Era field to Aero RS + RFE: Add context menu on build tab build table to add component to unit + RFE: Set Split Ratio for Splitable equipment + Bug: Claws showing as Tonnage/5 instead of Tonnage/7 for damage. + Added Tank UI + Added splash screens + RFE 2780128: Add delete to right click menu on build tab + Added new diamond image for Shield. + Updated code to use new MegaMek Equipment Flags + New MegaMek.jar

---
+++ src/megameklab/com/ui/Vehicle/tabs/ArmorTab.java
@@ -0,0 +1,257 @@
+/*
+* MegaMekLab - Copyright (C) 2009
+ *
+ * Original author - jtighe (torren@users.sourceforge.net)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+package megameklab.com.ui.Vehicle.tabs;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
+
+import megamek.common.CriticalSlot;
+import megamek.common.Entity;
+import megamek.common.EquipmentType;
+import megamek.common.Mounted;
+import megamek.common.Tank;
+import megamek.common.TechConstants;
+import megameklab.com.ui.Vehicle.views.ArmorView;
+import megameklab.com.util.ITab;
+import megameklab.com.util.RefreshListener;
+import megameklab.com.util.SpringLayoutHelper;
+import megameklab.com.util.UnitUtil;
+
+public class ArmorTab extends ITab implements ActionListener {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -7235362583437251408L;
+
+    private ArmorView armor;
+    private RefreshListener refresh = null;
+    private JComboBox armorCombo = new JComboBox(EquipmentType.armorNames);
+
+    private JButton allocateArmorButton = new JButton("Allocate");
+    private JButton maximizeArmorButton = new JButton("Maximize Armor");
+    private JSpinner armorTonnage = new JSpinner(new SpinnerNumberModel(0, 0, 0, 0.5));
+    private JCheckBox clanArmor = new JCheckBox("Clan Armor");
+
+    private JPanel buttonPanel = new JPanel();
+
+    public ArmorTab(Tank unit) {
+
+        this.unit = unit;
+        armor = new ArmorView(getTank());
+
+        Dimension comboSize = new Dimension(150, 20);
+
+        armorCombo.setMaximumSize(comboSize);
+        armorCombo.setPreferredSize(comboSize);
+        armorCombo.setMinimumSize(comboSize);
+
+        setLayout(new SpringLayout());
+        this.add(ButtonPanel());
+        this.add(armor);
+        SpringLayoutHelper.setupSpringGrid(this, 1);
+        setTotalTonnage();
+        addAllListeners();
+    }
+
+    public void refresh() {
+        removeAllListeners();
+        clanArmor.setVisible(unit.isMixedTech());
+        clanArmor.setSelected(unit.isClanArmor());
+        setTotalTonnage();
+        addAllListeners();
+        ((SpinnerNumberModel) armorTonnage.getModel()).setMaximum(UnitUtil.getMaximumArmorTonnage(unit));
+        armor.updateUnit(unit);
+        armor.refresh();
+    }
+
+    public void addRefreshedListener(RefreshListener l) {
+        refresh = l;
+        armor.addRefreshedListener(l);
+    }
+
+    public void actionPerformed(ActionEvent arg0) {
+        removeAllListeners();
+        if (arg0.getSource() instanceof JComboBox) {
+            unit.setArmorType(armorCombo.getSelectedIndex());
+            removeArmorMounts();
+            createArmorMounts();
+            if (refresh != null) {
+                refresh.refreshAll();
+            }
+        }
+        if (arg0.getSource().equals(allocateArmorButton)) {
+            armor.allocateArmor((Double) armorTonnage.getValue());
+        }
+        if (arg0.getSource().equals(maximizeArmorButton)) {
+            maximizeArmor();
+        }
+        if (arg0.getSource().equals(clanArmor)) {
+            if (clanArmor.isSelected()) {
+                if (unit.isClan()) {
+                    unit.setArmorTechLevel(unit.getTechLevel());
+                } else {
+                    unit.setArmorTechLevel(TechConstants.T_CLAN_TW);
+                }
+            } else if (unit.isClan()) {
+                unit.setArmorTechLevel(TechConstants.T_IS_TW_NON_BOX);
+            } else {
+                unit.setArmorTechLevel(unit.getTechLevel());
+            }
+            removeArmorMounts();
+            createArmorMounts();
+            if (refresh != null) {
+                refresh.refreshAll();
+            }
+        }
+        addAllListeners();
+    }
+
+    public JPanel ButtonPanel() {
+        JPanel masterPanel = new JPanel(new SpringLayout());
+
+        masterPanel.add(new JLabel("Armor", SwingConstants.TRAILING));
+        masterPanel.add(armorCombo);
+        masterPanel.add(clanArmor);
+
+        buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.Y_AXIS));
+
+        SpringLayoutHelper.setupSpringGrid(masterPanel, 3);
+
+        buttonPanel.add(masterPanel);
+        buttonPanel.setBorder(BorderFactory.createEtchedBorder(Color.WHITE.brighter(), Color.blue.darker()));
+
+        allocateArmorButton.addActionListener(this);
+
+        maximizeArmorButton.addActionListener(this);
+        clanArmor.addActionListener(this);
+
+        armorTonnage.setToolTipText("Total Tonnage of Armor");
+        Dimension size = new Dimension(45, 10);
+        armorTonnage.setMaximumSize(size);
+        armorTonnage.setPreferredSize(size);
+
+        JPanel sliderPanel = new JPanel(new SpringLayout());
+        sliderPanel.add(new JLabel("Armor Tonnage:", SwingConstants.TRAILING));
+        sliderPanel.add(armorTonnage);
+        sliderPanel.add(allocateArmorButton);
+        sliderPanel.add(maximizeArmorButton);
+        SpringLayoutHelper.setupSpringGrid(sliderPanel, 4);
+
+        buttonPanel.add(sliderPanel);
+
+        return buttonPanel;
+    }
+
+    private void addAllListeners() {
+        armorCombo.addActionListener(this);
+    }
+
+    private void removeAllListeners() {
+        armorCombo.removeActionListener(this);
+    }
+
+    private void maximizeArmor() {
+        double maxArmor = UnitUtil.getMaximumArmorTonnage(unit);
+        armor.allocateArmor(maxArmor);
+        armorTonnage.setValue(maxArmor);
+    }
+
+    private void createArmorMounts() {
+        int armorCount = 0;
+
+        armorCount = EquipmentType.get(EquipmentType.getArmorTypeName(unit.getArmorType())).getCriticals(unit);
+
+        if (armorCount < 1) {
+            return;
+        }
+
+        for (; armorCount > 0; armorCount--) {
+            try {
+                unit.addEquipment(EquipmentType.get(EquipmentType.getArmorTypeName(unit.getArmorType())), Entity.LOC_NONE, false);
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    private void removeArmorMounts() {
+
+        removeArmorCrits();
+
+        for (int pos = 0; pos < unit.getEquipment().size();) {
+            Mounted mount = unit.getEquipment().get(pos);
+            if (UnitUtil.isArmor(mount.getType())) {
+                unit.getEquipment().remove(pos);
+            } else {
+                pos++;
+            }
+        }
+
+        for (int pos = 0; pos < unit.getMisc().size();) {
+            Mounted mount = unit.getMisc().get(pos);
+            if (UnitUtil.isArmor(mount.getType())) {
+                unit.getMisc().remove(pos);
+            } else {
+                pos++;
+            }
+        }
+
+    }
+
+    private void removeArmorCrits() {
+
+        for (int location = 0; location < unit.locations(); location++) {
+            for (int slot = 0; slot < unit.getNumberOfCriticals(location); slot++) {
+                CriticalSlot crit = unit.getCritical(location, slot);
+                if ((crit != null) && (crit.getType() == CriticalSlot.TYPE_EQUIPMENT)) {
+                    Mounted mount = unit.getEquipment(crit.getIndex());
+
+                    if ((mount != null) && UnitUtil.isArmor(mount.getType())) {
+                        crit = null;
+                        unit.setCritical(location, slot, crit);
+                    }
+                }
+            }
+        }
+    }
+
+    private void setTotalTonnage() {
+        double currentTonnage = unit.getArmorWeight();
+        armorTonnage.setValue(currentTonnage);
+        armorTonnage.setToolTipText("Max Tonnage: " + UnitUtil.getMaximumArmorTonnage(unit));
+    }
+
+    public void setArmorType(int type) {
+        removeAllListeners();
+        armorCombo.setSelectedIndex(type);
+        addAllListeners();
+    }
+}
---
+++ src/megameklab/com/ui/Vehicle/tabs/EquipmentTab.java
@@ -0,0 +1,58 @@
+/*
+ * MegaMekLab - Copyright (C) 2009
+ *
+ * Original author - jtighe (torren@users.sourceforge.net)
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+package megameklab.com.ui.Vehicle.tabs;
+
+import javax.swing.SpringLayout;
+
+import megamek.common.Tank;
+import megameklab.com.ui.Vehicle.views.EquipmentView;
+import megameklab.com.util.ITab;
+import megameklab.com.util.RefreshListener;
+import megameklab.com.util.SpringLayoutHelper;
+
+public class EquipmentTab extends ITab {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 3978675469713289404L;
+
+    private EquipmentView equipment;
+
+    public EquipmentTab(Tank unit) {
+        this.unit = unit;
+        equipment = new EquipmentView(unit);
+        setLayout(new SpringLayout());
+        this.add(equipment);
+        SpringLayoutHelper.setupSpringGrid(this, 1);
+
+        refresh();
+    }
+
+    public void refresh() {
+        equipment.updateUnit(unit);
+        equipment.refresh();
+    }
+
+    public void addRefreshedListener(RefreshListener l) {
+        equipment.addRefreshedListener(l);
+    }
+
+    public EquipmentView getView() {
+        return equipment;
+    }
+}
