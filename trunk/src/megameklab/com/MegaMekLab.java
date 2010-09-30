6b2795e504a45f6583503f619b84ca2be1e6d22c
Torren <Torren@532ea136-1839-46d7-bdbd-97cf88639dd1>
2010-09-30 06:18:27 +0000

    + Added Mixed Tech Gyro's

--- trunk/src/megameklab/com/MegaMekLab.java
+++ trunk/src/megameklab/com/MegaMekLab.java
@@ -24,7 +24,7 @@ import java.io.PrintStream;
 import megameklab.com.ui.Mek.MainUI;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.0.0.17-Dev-108";
+    public static final String VERSION = "0.0.0.18-Dev-100";
 
     public static void main(String[] args) {
         String logFileName = "./logs/megameklab.log";
