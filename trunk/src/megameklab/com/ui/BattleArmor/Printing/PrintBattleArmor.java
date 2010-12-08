a226085c3bbf4581d585c66362ddfee7bff62919
beerockxs <beerockxs@532ea136-1839-46d7-bdbd-97cf88639dd1>
2010-12-08 15:06:38 +0000

    + new MM jar

--- trunk/src/megameklab/com/ui/BattleArmor/Printing/PrintBattleArmor.java
+++ trunk/src/megameklab/com/ui/BattleArmor/Printing/PrintBattleArmor.java
@@ -1,13 +1,13 @@
 /*
  * MegaMekLab - Copyright (C) 2009
- * 
+ *
  * Original author - jtighe (torren@users.sourceforge.net)
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
@@ -175,7 +175,7 @@ public class PrintBattleArmor implements Printable {
 
         font = UnitUtil.deriveFont(8.0f);
         g2d.setFont(font);
-        g2d.drawString(Integer.toString(battleArmor.getRunMP(true, true)), 79, 130 + currentMargin);
+        g2d.drawString(Integer.toString(battleArmor.getRunMP(true, true, false)), 79, 130 + currentMargin);
         int secondaryMP = battleArmor.getOriginalJumpMP();
         if (battleArmor.getMovementMode() == EntityMovementMode.INF_UMU) {
             secondaryMP = battleArmor.getRunMP();
