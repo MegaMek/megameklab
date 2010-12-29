9c280475375b14393f7d12ae2b525e63a94e58c4
beerockxs <beerockxs@532ea136-1839-46d7-bdbd-97cf88639dd1>
2010-12-29 20:52:57 +0000

    improved equipment tab

--- trunk/src/megameklab/com/util/EquipmentListCellRenderer.java
+++ trunk/src/megameklab/com/util/EquipmentListCellRenderer.java
@@ -16,18 +16,13 @@
 
 package megameklab.com.util;
 
-import java.awt.Color;
 import java.awt.Component;
 
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.JList;
-import javax.swing.border.Border;
-import javax.swing.border.EmptyBorder;
-
 import megamek.common.Entity;
 import megamek.common.EquipmentType;
 import megamek.common.TechConstants;
-import sun.swing.DefaultLookup;
 
 public class EquipmentListCellRenderer extends DefaultListCellRenderer {
 
@@ -35,9 +30,6 @@ public class EquipmentListCellRenderer extends DefaultListCellRenderer {
      *
      */
     private static final long serialVersionUID = 3787675734141978289L;
-
-    private static final Border SAFE_NO_FOCUS_BORDER = new EmptyBorder(1, 1, 1, 1);
-    private static final Border DEFAULT_NO_FOCUS_BORDER = new EmptyBorder(1, 1, 1, 1);
 
     private Entity unit;
 
@@ -48,6 +40,7 @@ public class EquipmentListCellRenderer extends DefaultListCellRenderer {
     @Override
     public Component getListCellRendererComponent(JList list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
 
+        Component comp = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
         StringBuffer text = new StringBuffer();
         EquipmentType etype = (EquipmentType) value;
         text.append(etype.getName());
@@ -58,58 +51,7 @@ public class EquipmentListCellRenderer extends DefaultListCellRenderer {
             text.append(" (Clan)");
         }
         setText(text.toString());
-        setComponentOrientation(list.getComponentOrientation());
-
-        Color bg = null;
-        Color fg = null;
-
-        JList.DropLocation dropLocation = list.getDropLocation();
-        if ((dropLocation != null) && !dropLocation.isInsert() && (dropLocation.getIndex() == index)) {
-
-            bg = DefaultLookup.getColor(this, ui, "List.dropCellBackground");
-            fg = DefaultLookup.getColor(this, ui, "List.dropCellForeground");
-
-            isSelected = true;
-        }
-        if (isSelected) {
-            setBackground(bg == null ? list.getSelectionBackground() : bg);
-            setForeground(fg == null ? list.getSelectionForeground() : fg);
-
-        } else {
-            setBackground(list.getBackground());
-            setForeground(list.getForeground());
-        }
-        setEnabled(list.isEnabled());
-        setFont(list.getFont());
-
-        Border border = null;
-        if (cellHasFocus) {
-            if (isSelected) {
-                border = DefaultLookup.getBorder(this, ui, "List.focusSelectedCellHighlightBorder");
-            }
-            if (border == null) {
-                border = DefaultLookup.getBorder(this, ui, "List.focusCellHighlightBorder");
-            }
-        } else {
-            border = getNoFocusBorder();
-        }
-        setBorder(border);
-
-        return this;
+        return comp;
     }
 
-    private Border getNoFocusBorder() {
-        Border border = DefaultLookup.getBorder(this, ui, "List.cellNoFocusBorder");
-        if (System.getSecurityManager() != null) {
-            if (border != null) {
-                return border;
-            }
-            return SAFE_NO_FOCUS_BORDER;
-        } else {
-            if ((border != null) && ((noFocusBorder == null) || (noFocusBorder == DEFAULT_NO_FOCUS_BORDER))) {
-                return border;
-            }
-            return noFocusBorder;
-        }
-    }
 }
