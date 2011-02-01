559836df102a14b821bf04fb6354bf5cec7f8f9e
beerockxs <sebastian@brocks.cc>
2011-02-01 22:22:19 +0000

    + Vehicle printing equipment font size issue

--- src/megameklab/com/util/ImageHelperVehicle.java
+++ src/megameklab/com/util/ImageHelperVehicle.java
@@ -1,13 +1,13 @@
 /*
  * MegaMekLab - Copyright (C) 2010
- * 
+ *
  * Original author - jtighe (torren@users.sourceforge.net)
- * 
+ *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
@@ -263,6 +263,7 @@ public class ImageHelperVehicle {
                 if (eqi.isSponsonMounted) {
                     location += "po";
                 }
+                g2d.setFont(font);
                 g2d.drawString(location, locPoint, linePoint);
                 if (eqi.isWeapon) {
                     if (eqi.isAMS) {
