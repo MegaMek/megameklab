bcc6cd6eddca18eccbe0f7d447640192169c5a4e
beerockxs <beerockxs@532ea136-1839-46d7-bdbd-97cf88639dd1>
2012-07-29 17:58:49 +0000

    update for MM changes

--- trunk/src/megameklab/com/ui/Vehicle/views/BuildView.java
+++ trunk/src/megameklab/com/ui/Vehicle/views/BuildView.java
@@ -107,7 +107,7 @@ public class BuildView extends IView implements ActionListener, MouseListener {
             }
         }
         for (Mounted mount : unit.getAmmo()) {
-            if ((mount.getLocation() == Entity.LOC_NONE) && (mount.getShotsLeft() > 1)) {
+            if ((mount.getLocation() == Entity.LOC_NONE) && (mount.getUsableShotsLeft() > 1)) {
                 masterEquipmentList.add(mount);
             }
         }
