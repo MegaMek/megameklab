11a3e0b046d422626712123636d2d58191ace36f
beerockxs <sebastian@brocks.cc>
2013-06-21 18:06:51 +0000

    updated MM.jar

--- src/megameklab/com/ui/Mek/views/SummaryView.java
+++ src/megameklab/com/ui/Mek/views/SummaryView.java
@@ -33,9 +33,8 @@ import megamek.common.EquipmentType;
 import megamek.common.Mech;
 import megamek.common.MiscType;
 import megamek.common.Mounted;
-import megamek.common.WeaponType;
+import megamek.common.TechConstants;
 import megamek.common.verifier.EntityVerifier;
-import megamek.common.verifier.TestEntity;
 import megamek.common.verifier.TestMech;
 import megameklab.com.util.IView;
 import megameklab.com.util.UnitUtil;
@@ -43,10 +42,10 @@ import megameklab.com.util.UnitUtil;
 public class SummaryView extends IView{
 
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = -8492419359401770037L;
-    
+
     private JTextField txtStructTon = new JTextField("?");
     private JTextField txtEngineTon = new JTextField("?");
     private JTextField txtGyroTon = new JTextField("?");
@@ -57,7 +56,7 @@ public class SummaryView extends IView{
     private JTextField txtJumpTon = new JTextField("?");
     private JTextField txtEquipTon = new JTextField("?");
     private JTextField txtOtherTon = new JTextField("?");
-    
+
     private JTextField txtStructCrit = new JTextField("?");
     private JTextField txtEngineCrit = new JTextField("?");
     private JTextField txtGyroCrit = new JTextField("?");
@@ -68,7 +67,7 @@ public class SummaryView extends IView{
     private JTextField txtJumpCrit = new JTextField("?");
     private JTextField txtEquipCrit = new JTextField("?");
     private JTextField txtOtherCrit = new JTextField("-");
-    
+
     private JTextField txtStructAvail = new JTextField("?");
     private JTextField txtEngineAvail = new JTextField("?");
     private JTextField txtGyroAvail = new JTextField("?");
@@ -81,10 +80,10 @@ public class SummaryView extends IView{
     private JTextField txtOtherAvail = new JTextField("?");
 
     private EntityVerifier entityVerifier = new EntityVerifier(new File("data/mechfiles/UnitVerifierOptions.xml"));
-    
+
     public SummaryView(Mech unit) {
         super(unit);
-    
+
         Vector<JTextField> valueFields = new Vector<JTextField>();
 
         valueFields.add(txtStructTon);
@@ -97,7 +96,7 @@ public class SummaryView extends IView{
         valueFields.add(txtJumpTon);
         valueFields.add(txtEquipTon);
         valueFields.add(txtOtherTon);
-        
+
         valueFields.add(txtStructCrit);
         valueFields.add(txtEngineCrit);
         valueFields.add(txtGyroCrit);
@@ -116,11 +115,11 @@ public class SummaryView extends IView{
             field.setPreferredSize(size);
             field.setMinimumSize(size);
             field.setMaximumSize(size);
-            field.setHorizontalAlignment(JTextField.RIGHT);
+            field.setHorizontalAlignment(SwingConstants.RIGHT);
         }
-        
+
         valueFields.removeAllElements();
-        
+
         valueFields.add(txtStructAvail);
         valueFields.add(txtEngineAvail);
         valueFields.add(txtGyroAvail);
@@ -140,42 +139,42 @@ public class SummaryView extends IView{
             field.setMinimumSize(size);
             field.setMaximumSize(size);
         }
-        
-        this.setLayout(new GridBagLayout());
+
+        setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        
+
         size = new Dimension(120,25);
 
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.insets = new Insets(0,0,0,5);
-        this.add(createLabel("Category", size, JLabel.CENTER), gbc);
+        this.add(createLabel("Category", size, SwingConstants.CENTER), gbc);
         gbc.gridy = 1;
-        this.add(createLabel("Internal Structure:", size, JLabel.RIGHT), gbc);
+        this.add(createLabel("Internal Structure:", size, SwingConstants.RIGHT), gbc);
         gbc.gridy = 2;
-        this.add(createLabel("Engine:", size, JLabel.RIGHT), gbc);
+        this.add(createLabel("Engine:", size, SwingConstants.RIGHT), gbc);
         gbc.gridy = 3;
-        this.add(createLabel("Gyro:", size, JLabel.RIGHT), gbc);
+        this.add(createLabel("Gyro:", size, SwingConstants.RIGHT), gbc);
         gbc.gridy = 4;
-        this.add(createLabel("Cockpit:", size, JLabel.RIGHT), gbc);
+        this.add(createLabel("Cockpit:", size, SwingConstants.RIGHT), gbc);
         gbc.gridy = 5;
-        this.add(createLabel("Heat Sinks:", size, JLabel.RIGHT), gbc);
+        this.add(createLabel("Heat Sinks:", size, SwingConstants.RIGHT), gbc);
         gbc.gridy = 6;
-        this.add(createLabel("Armor:", size, JLabel.RIGHT), gbc);
+        this.add(createLabel("Armor:", size, SwingConstants.RIGHT), gbc);
         gbc.gridy = 7;
-        this.add(createLabel("Enhancements:", size, JLabel.RIGHT), gbc);
+        this.add(createLabel("Enhancements:", size, SwingConstants.RIGHT), gbc);
         gbc.gridy = 8;
-        this.add(createLabel("Jump Jets:", size, JLabel.RIGHT), gbc);
+        this.add(createLabel("Jump Jets:", size, SwingConstants.RIGHT), gbc);
         gbc.gridy = 9;
-        this.add(createLabel("Equipment", size, JLabel.RIGHT), gbc);
+        this.add(createLabel("Equipment", size, SwingConstants.RIGHT), gbc);
         gbc.gridy = 10;
-        this.add(createLabel("Other:", size, JLabel.RIGHT), gbc);
-        
+        this.add(createLabel("Other:", size, SwingConstants.RIGHT), gbc);
+
         size = new Dimension(45,25);
         gbc.gridx = 1;
         gbc.gridy = 0;
         gbc.insets = new Insets(0,0,0,0);
-        this.add(createLabel("Ton", size, JLabel.CENTER), gbc);
+        this.add(createLabel("Ton", size, SwingConstants.CENTER), gbc);
         gbc.gridy = 1;
         this.add(txtStructTon, gbc);
         gbc.gridy = 2;
@@ -196,10 +195,10 @@ public class SummaryView extends IView{
         this.add(txtEquipTon, gbc);
         gbc.gridy = 10;
         this.add(txtOtherTon, gbc);
-        
+
         gbc.gridx = 2;
         gbc.gridy = 0;
-        this.add(createLabel("Crit", size, JLabel.CENTER), gbc);
+        this.add(createLabel("Crit", size, SwingConstants.CENTER), gbc);
         gbc.gridy = 1;
         this.add(txtStructCrit, gbc);
         gbc.gridy = 2;
@@ -220,11 +219,11 @@ public class SummaryView extends IView{
         this.add(txtEquipCrit, gbc);
         gbc.gridy = 10;
         this.add(txtOtherCrit, gbc);
-        
+
         size = new Dimension(80,25);
         gbc.gridx = 3;
         gbc.gridy = 0;
-        this.add(createLabel("Availability", size, JLabel.CENTER), gbc);
+        this.add(createLabel("Availability", size, SwingConstants.CENTER), gbc);
         gbc.gridy = 1;
         this.add(txtStructAvail, gbc);
         gbc.gridy = 2;
@@ -245,11 +244,11 @@ public class SummaryView extends IView{
         this.add(txtEquipAvail, gbc);
         gbc.gridy = 10;
         this.add(txtOtherAvail, gbc);
-        
-        this.setBorder(BorderFactory.createTitledBorder("Summary"));
-        
+
+        setBorder(BorderFactory.createTitledBorder("Summary"));
+
     }
-        
+
     private JLabel createLabel(String text, Dimension size, int align) {
 
         JLabel label = new JLabel(text, SwingConstants.TRAILING);
@@ -258,17 +257,17 @@ public class SummaryView extends IView{
         label.setHorizontalAlignment(align);
         return label;
     }
-    
+
     public void setFieldSize(JComponent box, Dimension maxSize) {
         box.setPreferredSize(maxSize);
         box.setMaximumSize(maxSize);
         box.setMinimumSize(maxSize);
     }
-    
+
     public void refresh() {
-        
+
         TestMech testMech = new TestMech(getMech(), entityVerifier.mechOption, null);
-        
+
         txtGyroTon.setText(Float.toString(testMech.getWeightGyro()));
         txtEngineTon.setText(Float.toString(testMech.getWeightEngine()));
         txtCockpitTon.setText(Float.toString(testMech.getWeightCockpit()));
@@ -277,16 +276,16 @@ public class SummaryView extends IView{
         txtArmorTon.setText(Float.toString(testMech.getWeightArmor()));
         txtOtherTon.setText(Float.toString(testMech.getWeightPowerAmp() + testMech.getWeightCarryingSpace() + testMech.getWeightMisc()));
 
-        
+
         txtGyroCrit.setText(Integer.toString(getGyroCrits()));
         txtEngineCrit.setText(Integer.toString(getEngineCrits()));
         txtCockpitCrit.setText(Integer.toString(getCockpitCrits()));
-        String structName = EquipmentType.getStructureTypeName(getMech().getStructureType());
+        String structName = EquipmentType.getStructureTypeName(getMech().getStructureType(),getMech().isClan());
         txtStructCrit.setText(Integer.toString(EquipmentType.get(structName).getCriticals(getMech())));
-        txtArmorCrit.setText(Integer.toString(EquipmentType.get(EquipmentType.getArmorTypeName(unit.getArmorType(0))).getCriticals(unit)));
-        
+        txtArmorCrit.setText(Integer.toString(EquipmentType.get(EquipmentType.getArmorTypeName(unit.getArmorType(0),TechConstants.isClan(unit.getArmorTechLevel(0)))).getCriticals(unit)));
+
         runThroughEquipment(testMech);
-        
+
         int numberSinks = UnitUtil.countActualHeatSinks(getMech());
         numberSinks = Math.max(0, numberSinks - UnitUtil.getBaseChassisHeatSinks(getMech(), getMech().hasCompactHeatSinks()));
         int critSinks = numberSinks;
@@ -297,13 +296,13 @@ public class SummaryView extends IView{
             critSinks = numberSinks * 3;
         }
         else if(getMech().hasCompactHeatSinks()) {
-            critSinks = critSinks/2 + critSinks%2;
+            critSinks = (critSinks/2) + (critSinks%2);
         }
         txtHeatCrit.setText(Integer.toString(critSinks));
 
-    
+
     }
-    
+
     private void runThroughEquipment(TestMech testMech) {
         float weightJJ = 0.0f;
         float weightEnhance = 0.0f;
@@ -311,12 +310,12 @@ public class SummaryView extends IView{
         int critJJ = 0;
         int critEquip = 0;
         int critEnhance = 0;
-        
+
         for (Mounted m : getMech().getMisc()) {
             MiscType mt = (MiscType) m.getType();
             if(UnitUtil.isArmorOrStructure(mt)) {
                 continue;
-            } 
+            }
             else if (mt.hasFlag(MiscType.F_TSM)
                     || mt.hasFlag(MiscType.F_INDUSTRIAL_TSM)
                     || mt.hasFlag(MiscType.F_MASC)) {
@@ -338,24 +337,24 @@ public class SummaryView extends IView{
             }
         }
         for (Mounted m : getMech().getWeaponList()) {
-            EquipmentType et = (EquipmentType) m.getType();
+            EquipmentType et = m.getType();
             weightEquip += et.getTonnage(getMech());
             critEquip += et.getCriticals(getMech());
         }
         for (Mounted m : getMech().getAmmo()) {
-            EquipmentType et = (EquipmentType) m.getType();
+            EquipmentType et = m.getType();
             weightEquip += et.getTonnage(getMech());
             critEquip += et.getCriticals(getMech());
         }
         txtJumpTon.setText(Float.toString(weightJJ));
         txtEnhanceTon.setText(Float.toString(weightEnhance));
         txtEquipTon.setText(Float.toString(weightEquip));
-        
+
         txtJumpCrit.setText(Integer.toString(critJJ));
         txtEnhanceCrit.setText(Integer.toString(critEnhance));
         txtEquipCrit.setText(Integer.toString(critEquip));
     }
-    
+
     private int getGyroCrits() {
         switch(getMech().getGyroType()) {
         case Mech.GYRO_COMPACT:
@@ -366,11 +365,11 @@ public class SummaryView extends IView{
             return 4;
         }
     }
-    
+
     private int getEngineCrits() {
-        return 6 + 2 * getMech().getEngine().getSideTorsoCriticalSlots().length;
+        return 6 + (2 * getMech().getEngine().getSideTorsoCriticalSlots().length);
     }
-    
+
     private int getCockpitCrits() {
         switch(getMech().getCockpitType()) {
         case Mech.COCKPIT_COMMAND_CONSOLE:
