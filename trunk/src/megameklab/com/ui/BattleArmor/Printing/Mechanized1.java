74986d0ddd3a9b758d0b4eb08f819440e7108a5b
arlith <arlith@532ea136-1839-46d7-bdbd-97cf88639dd1>
2014-02-08 15:16:46 +0000

    + Removed more unused imports and local variables

--- trunk/src/megameklab/com/ui/BattleArmor/Printing/Mechanized1.java
+++ trunk/src/megameklab/com/ui/BattleArmor/Printing/Mechanized1.java
@@ -39,7 +39,7 @@ public class Mechanized1 {
     public static void paint(Graphics2D g) {
         Shape shape = null;
         Paint paint = null;
-        Stroke stroke = null;
+        //Stroke stroke = null;
         Area clip = null;
         
         float origAlpha = 1.0f;
