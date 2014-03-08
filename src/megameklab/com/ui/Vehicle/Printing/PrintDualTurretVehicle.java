8883bc6ac3f59e4b42b9105a9cb5a79da2c34720
beerockxs <sebastian@brocks.cc>
2014-03-08 19:42:40 +0000

    fix printing for vees <= 5 tons

--- src/megameklab/com/ui/Vehicle/Printing/PrintDualTurretVehicle.java
+++ src/megameklab/com/ui/Vehicle/Printing/PrintDualTurretVehicle.java
@@ -197,7 +197,7 @@ public class PrintDualTurretVehicle implements Printable {
             // unusualSymbols.setGroupingSeparator(',');
             // DecimalFormat myFormatter = new DecimalFormat("#.###",
             // unusualSymbols);
-            g2d.drawString(String.format("%1$,d", tank.getWeight()), 177, 134);
+            g2d.drawString(String.format("%1$,.1f", tank.getWeight()), 177, 134);
         }
 
         int nextDataLine = 153;
