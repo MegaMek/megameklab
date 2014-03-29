f2d8e524bfbaf51136e83398ecea813643564734
ralgith-erian <ralgith-erian@532ea136-1839-46d7-bdbd-97cf88639dd1>
2014-03-29 15:13:47 +0000

    + New MegaMek.jar 0.37.9 (0.38.0 RC1) + development snapshot <0.1.26>

--- trunk/src/megameklab/com/MegaMekLab.java
+++ trunk/src/megameklab/com/MegaMekLab.java
@@ -25,7 +25,7 @@ import java.util.Locale;
 import megameklab.com.ui.Mek.MainUI;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.1.26-dev";
+    public static final String VERSION = "0.1.26";
 
     public static void main(String[] args) {
     	System.setProperty("apple.laf.useScreenMenuBar", "true");
