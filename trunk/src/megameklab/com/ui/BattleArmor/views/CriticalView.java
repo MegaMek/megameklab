74551e87be27addf3a3e6e028ca0c99d81ccf44b
arlith <arlith@532ea136-1839-46d7-bdbd-97cf88639dd1>
2015-01-20 03:39:55 +0000

    + Bug [#160]: MML can no longer add equipment to the arms of Battle Armor

--- trunk/src/megameklab/com/ui/BattleArmor/views/CriticalView.java
+++ trunk/src/megameklab/com/ui/BattleArmor/views/CriticalView.java
@@ -42,6 +42,7 @@ import megamek.common.loaders.MtfFile;
 import megamek.common.verifier.EntityVerifier;
 import megamek.common.verifier.TestBattleArmor;
 import megamek.common.weapons.infantry.InfantryWeapon;
+import megameklab.com.ui.BattleArmor.CriticalSuit;
 import megameklab.com.util.IView;
 import megameklab.com.util.RefreshListener;
 import megameklab.com.util.Mech.DropTargetCriticalList;
@@ -326,98 +327,5 @@ public class CriticalView extends IView {
         box.setPreferredSize(maxSize);
         box.setMaximumSize(maxSize);
         box.setMinimumSize(maxSize);
-    }
-    
-    /**
-     * Since BattleArmor is setup in a squad, the standard CriticalSlot system
-     * isn't used.  For construction purposes, we keep track of criticals.
-     **/
-    private class CriticalSuit {
-        
-        //store critical slots just like an entity
-        protected CriticalSlot[][] crits; // [loc][slot]
-        
-        BattleArmor ba;
-
-        public CriticalSuit(BattleArmor ba) {
-            this.ba = ba;
-            crits = new CriticalSlot[locations()][];
-            for (int i = 0; i < locations(); i++) {
-                int numSlots;
-                switch (i) {
-                case BattleArmor.MOUNT_LOC_BODY:
-                    crits[i] = new CriticalSlot[ba.getBodyCrits()];
-                    break;
-                case BattleArmor.MOUNT_LOC_RARM:
-                    numSlots = ba.getArmCrits();
-                    if (ba.getRightManipulator() != null) {
-                        numSlots++;
-                    }
-                    crits[i] = new CriticalSlot[numSlots];
-                    break;
-                case BattleArmor.MOUNT_LOC_LARM:
-                    numSlots = ba.getArmCrits();
-                    if (ba.getLeftManipulator() != null) {
-                        numSlots++;
-                    }
-                    crits[i] = new CriticalSlot[numSlots];
-                    break;
-                }
-
-            }
-        }
-        
-        public int locations() {
-            return BattleArmor.MOUNT_NUM_LOCS;
-        }
-
-        public int getNumCriticals(int loc) {
-            return crits[loc].length;
-        }
-        
-        public void addMounted(int loc, Mounted m){
-            // Don't mount unmounted equipment
-            if (loc == BattleArmor.MOUNT_LOC_NONE){
-                return;
-            }
-            
-            // AP Weapons that are mounted in an AP Mount don't take up slots
-            if (m.isAPMMounted() && m.getLinkedBy() != null 
-                    && m.getLinkedBy().getType().hasFlag(MiscType.F_AP_MOUNT)){
-                return;
-            }
-            
-            // Manipulators will always go in the last slot in its location,
-            //  as they get a special slot added for them
-            if (m.getType().hasFlag(MiscType.F_BA_MANIPULATOR)){
-                int slot = crits[loc].length - 1;
-                crits[loc][slot] = new CriticalSlot(m);
-            }
-            
-            int critsToAdd;
-            if (m.getType().isSpreadable()){
-                critsToAdd = 1;
-            } else {
-                critsToAdd = m.getType().getCriticals(ba);
-            }
-            if (critsToAdd == 0){
-                return;
-            }
-            for (int slot = 0; slot < getNumCriticals(loc); slot++){
-                if (crits[loc][slot] == null){
-                    crits[loc][slot] = new CriticalSlot(m);
-                    critsToAdd--;
-                    if (critsToAdd <= 0){
-                        break;
-                    }
-                }
-            }
-        }
-        
-        public CriticalSlot getCritical(int loc, int slot) {
-            return crits[loc][slot];
-        }
-        
-        
     }
 }
