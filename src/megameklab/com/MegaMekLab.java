88afb9ba5bbfdd01248c87290615fb0b13f5f1fc
HammerGS <drivingguy@hotmail.com>
2016-05-27 14:54:04 -0600

    ready to develop <0.1.55>

--- src/megameklab/com/MegaMekLab.java
+++ src/megameklab/com/MegaMekLab.java
@@ -25,7 +25,7 @@ import java.util.Locale;
 import megameklab.com.ui.Mek.MainUI;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.1.54";
+    public static final String VERSION = "0.1.55-git";
 
     public static void main(String[] args) {
     	System.setProperty("apple.laf.useScreenMenuBar", "true");
