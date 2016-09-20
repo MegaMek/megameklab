1b6e68ed6052efacca1ba76d3ee58ebe068cfb88
Dylan Myers <ralgith@gmail.com>
2016-09-19 20:30:46 -0400

    + ready for development <0.41.24>

--- src/megameklab/com/MegaMekLab.java
+++ src/megameklab/com/MegaMekLab.java
@@ -25,7 +25,7 @@ import java.util.Locale;
 import megameklab.com.ui.Mek.MainUI;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.1.59";
+    public static final String VERSION = "0.41.24-git";
 
     public static void main(String[] args) {
     	System.setProperty("apple.laf.useScreenMenuBar", "true");
