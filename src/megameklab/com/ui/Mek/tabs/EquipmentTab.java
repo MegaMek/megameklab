064c84e265fae8db6eed623a13e23be73ace4cf1
Carl Spain <neoancient@megamek.org>
2017-06-24 13:53:38 -0500

    Filter QuadVee wheels out of equipment table and for QuadVees also filter out tracks.

--- src/megameklab/com/ui/Mek/tabs/EquipmentTab.java
+++ src/megameklab/com/ui/Mek/tabs/EquipmentTab.java
@@ -62,6 +62,7 @@ import megamek.common.LocationFullException;
 import megamek.common.Mech;
 import megamek.common.MiscType;
 import megamek.common.Mounted;
+import megamek.common.QuadVee;
 import megamek.common.WeaponType;
 import megamek.common.weapons.ArtilleryWeapon;
 import megameklab.com.ui.EntitySource;
@@ -551,6 +552,13 @@ public class EquipmentTab extends ITab implements ActionListener {
                 }
                 if ((etype instanceof MiscType) && (etype.hasFlag(MiscType.F_TSM) || etype.hasFlag(MiscType.F_INDUSTRIAL_TSM) || (etype.hasFlag(MiscType.F_MASC) && !etype.hasSubType(MiscType.S_SUPERCHARGER)))) {
                     return false;
+                }
+                if (etype instanceof MiscType && etype.hasFlag(MiscType.F_TRACKS)) {
+                    if (getMech() instanceof QuadVee) {
+                        return false;
+                    } else if (etype.hasSubType(MiscType.S_QUADVEE_WHEELS)) {
+                        return false;
+                    }
                 }
                 if (((nType == T_OTHER) && UnitUtil.isMechEquipment(etype, mech))
                         || (((nType == T_WEAPON) && (UnitUtil.isMechWeapon(etype, mech) || UnitUtil.isPhysicalWeapon(etype))))
