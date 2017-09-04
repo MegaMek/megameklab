5fc37f3afb1f98bd7c8d0476dece3f8e3c221ab8
Carl Spain <neoancient@megamek.org>
2017-09-04 10:10:00 -0500

    Added armor view for units that assign armor by point (BA/Protomech). Show only the distinct manipulator name in the manipulators combo box.

--- src/megameklab/com/ui/BattleArmor/tabs/StructureTab.java
+++ src/megameklab/com/ui/BattleArmor/tabs/StructureTab.java
@@ -32,12 +32,7 @@ import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSpinner;
-import javax.swing.JTextField;
-import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 import megamek.client.ui.swing.MechViewPanel;
 import megamek.common.BattleArmor;
@@ -48,12 +43,12 @@ import megamek.common.MechView;
 import megamek.common.MiscType;
 import megamek.common.Mounted;
 import megamek.common.SimpleTechLevel;
-import megamek.common.TechConstants;
-import megamek.common.verifier.TestBattleArmor;
 import megamek.common.verifier.TestBattleArmor.BAManipulator;
 import megameklab.com.ui.EntitySource;
+import megameklab.com.ui.util.CustomComboBox;
 import megameklab.com.ui.view.BAChassisView;
 import megameklab.com.ui.view.BAEnhancementView;
+import megameklab.com.ui.view.BAProtoArmorView;
 import megameklab.com.ui.view.BasicInfoView;
 import megameklab.com.ui.view.ITechManager;
 import megameklab.com.ui.view.MovementView;
@@ -61,10 +56,11 @@ import megameklab.com.util.ITab;
 import megameklab.com.util.RefreshListener;
 import megameklab.com.util.UnitUtil;
 
-public class StructureTab extends ITab implements ActionListener, ChangeListener,
+public class StructureTab extends ITab implements ActionListener,
         BasicInfoView.BasicInfoListener,
         BAChassisView.ChassisListener,
         MovementView.MovementListener,
+        BAProtoArmorView.ArmorListener,
         BAEnhancementView.EnhancementListener {
 
 	/**
@@ -80,19 +76,12 @@ public class StructureTab extends ITab implements ActionListener, ChangeListener
     private BasicInfoView panBasicInfo;
     private BAChassisView panChassis;
     private MovementView panMovement;
+    private BAProtoArmorView panArmor;
     private BAEnhancementView panEnhancements;
     
-    // Armor Panel
-    private JComboBox<String> armorType;
-    
-    private JSpinner armorPoints;
-    
     // Manipulator Panel
-    private JComboBox<String> leftManipSelect = new JComboBox<String>();
-    private JComboBox<String> rightManipSelect = new JComboBox<String>();
-    
-    private JTextField txtPrimary = new JTextField("None");
-    private JTextField txtSecondary = new JTextField("None");
+    private CustomComboBox<String> leftManipSelect = new CustomComboBox<>(s -> manipulatorDisplayName(s));
+    private CustomComboBox<String> rightManipSelect = new CustomComboBox<>(s -> manipulatorDisplayName(s));
     
     private MechViewPanel panelMekView;
     
@@ -120,42 +109,15 @@ public class StructureTab extends ITab implements ActionListener, ChangeListener
         panBasicInfo = new BasicInfoView(getBattleArmor().getConstructionTechAdvancement());
         panChassis = new BAChassisView(panBasicInfo);
         panMovement = new MovementView(panBasicInfo);
-        JPanel armorPanel = new JPanel(new GridBagLayout());
+        panArmor = new BAProtoArmorView(panBasicInfo);
         JPanel manipPanel = new JPanel(new GridBagLayout());
         panEnhancements = new BAEnhancementView(panBasicInfo);
         GridBagConstraints gbc = new GridBagConstraints();
         Dimension comboSize = new Dimension(250, 25);
        
-        Dimension spinnerSize = new Dimension(55, 25);
-
-        txtPrimary.setEditable(false);
-        txtSecondary.setEditable(false);
-
         gbc.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gbc.anchor = GridBagConstraints.WEST;
 
-        armorType = new JComboBox<String>();
-        armorPoints = new JSpinner(new SpinnerNumberModel(0, 0, getBattleArmor().getMaximumArmorPoints(), 1));
-        ((JSpinner.DefaultEditor) armorPoints.getEditor()).setSize(spinnerSize);
-        ((JSpinner.DefaultEditor) armorPoints.getEditor())
-                .setMaximumSize(spinnerSize);
-        ((JSpinner.DefaultEditor) armorPoints.getEditor())
-                .setPreferredSize(spinnerSize);
-        ((JSpinner.DefaultEditor) armorPoints.getEditor())
-                .setMinimumSize(spinnerSize);
-        ((JSpinner.DefaultEditor) armorPoints.getEditor()).getTextField()
-                .setEditable(false);
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        armorPanel.add(createLabel("Armor Type:", labelSize), gbc);
-        gbc.gridx = 1;
-        armorPanel.add(armorType, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        armorPanel.add(createLabel("Amount:", labelSize), gbc);
-        gbc.gridx = 1;
-        armorPanel.add(armorPoints, gbc);
-        
         gbc.gridx = 0; 
         gbc.gridy = 0;
         gbc.anchor = GridBagConstraints.NORTHWEST;
@@ -170,18 +132,13 @@ public class StructureTab extends ITab implements ActionListener, ChangeListener
         gbc.gridy = 1;
         manipPanel.add(rightManipSelect, gbc);
         
-
-        setFieldSize(armorType, comboSize);
-        setFieldSize(txtPrimary, comboSize);
-        setFieldSize(txtSecondary, comboSize);
         setFieldSize(leftManipSelect, comboSize);
         setFieldSize(rightManipSelect, comboSize);
-
 
         panBasicInfo.setBorder(BorderFactory.createTitledBorder("Basic Information"));
         panChassis.setBorder(BorderFactory.createTitledBorder("Chassis"));
         panMovement.setBorder(BorderFactory.createTitledBorder("Movement"));
-        armorPanel.setBorder(BorderFactory.createTitledBorder("Armor"));
+        panArmor.setBorder(BorderFactory.createTitledBorder("Armor"));
         //weaponView.setBorder(BorderFactory.createTitledBorder("Weapon Selection"));
         manipPanel.setBorder(BorderFactory.createTitledBorder("Manipulators"));
         panEnhancements.setBorder(BorderFactory.createTitledBorder("Enhancements"));
@@ -189,14 +146,13 @@ public class StructureTab extends ITab implements ActionListener, ChangeListener
         leftPanel.add(panBasicInfo);
         leftPanel.add(panChassis);
         leftPanel.add(panMovement);
-        leftPanel.add(armorPanel);
+        leftPanel.add(panArmor);
         leftPanel.add(manipPanel);
         //leftPanel.add(Box.createVerticalGlue());
         
         rightPanel.add(Box.createVerticalStrut(5));
         rightPanel.add(previewPanel);
         rightPanel.add(panEnhancements);
-        
         
         setLayout(new GridBagLayout());
         gbc = new GridBagConstraints();
@@ -239,64 +195,11 @@ public class StructureTab extends ITab implements ActionListener, ChangeListener
 	    panBasicInfo.setFromEntity(getBattleArmor());
 	    panChassis.setFromEntity(getBattleArmor());
 	    panMovement.setFromEntity(getBattleArmor());
+        panArmor.setFromEntity(getBattleArmor());
 	    panEnhancements.setFromEntity(getBattleArmor());
 	    
 	    removeAllListeners();
 	    
-        boolean isMixed = getBattleArmor().isMixedTech();
-        boolean isExperimental = (getBattleArmor().getTechLevel() == TechConstants.T_IS_EXPERIMENTAL)
-                || (getBattleArmor().getTechLevel() == TechConstants.T_CLAN_EXPERIMENTAL)
-                || (getBattleArmor().getTechLevel() == TechConstants.T_IS_UNOFFICIAL)
-                || (getBattleArmor().getTechLevel() == TechConstants.T_CLAN_UNOFFICIAL);
-	    
-
-        
-        armorType.removeAllItems();
-        for (TestBattleArmor.BAArmor armor : TestBattleArmor.BAArmor.values()){
-            int type = armor.type;
-            EquipmentType et;
-            if (!isMixed) {
-                boolean techMatch = (armor.isClan && getBattleArmor().isClan()) || 
-                        (!armor.isClan && !getBattleArmor().isClan());
-                et = EquipmentType.get(
-                        EquipmentType.getArmorTypeName(type, armor.isClan));
-                boolean legalTechLvl = (et != null) &&  
-                        (TechConstants.isLegal(getBattleArmor().getTechLevel(), 
-                                et.getTechLevel(getBattleArmor().getYear()), 
-                                isMixed));
-                if (techMatch && (isExperimental || legalTechLvl)) {
-                    armorType.addItem(EquipmentType.armorNames[type]);
-                }
-            } else {
-                String armorName = 
-                        EquipmentType.getArmorTypeName(type, armor.isClan);
-                et = EquipmentType.get(armorName);
-                if (et != null && TechConstants.isLegal(getBattleArmor().getTechLevel(), 
-                                et.getTechLevel(getBattleArmor().getYear()), 
-                                isMixed)) {
-                    armorType.addItem(armorName);
-                }
-            }
-        }           
-        
-        String armorName = "";
-        if (isMixed){
-            boolean isClanArmor = TechConstants.isClan(
-                    getBattleArmor().getArmorTechLevel(BattleArmor.LOC_SQUAD));
-            if (isClanArmor){
-                armorName = "Clan ";
-            } else {
-                armorName = "IS ";
-            }
-        }
-        armorName += EquipmentType.getArmorTypeName(
-                getBattleArmor().getArmorType(BattleArmor.LOC_SQUAD));
-        armorType.setSelectedItem(armorName);
-        
-        // We have to use a LOC_TROOPER as getOArmor has a special case for LOC_SQUAD...
-        armorPoints.setValue(getBattleArmor().getOArmor(BattleArmor.LOC_TROOPER_1));
-        ((SpinnerNumberModel) armorPoints.getModel()).setMaximum(getBattleArmor().getMaximumArmorPoints());
-
         // Manipulators
         leftManipSelect.removeAllItems();
         rightManipSelect.removeAllItems();
@@ -304,11 +207,7 @@ public class StructureTab extends ITab implements ActionListener, ChangeListener
         rightManipSelect.addItem(BattleArmor.MANIPULATOR_TYPE_STRINGS[BattleArmor.MANIPULATOR_NONE]);
         for (BAManipulator manip : BAManipulator.values()){
             EquipmentType et = EquipmentType.get(manip.internalName);
-            boolean legalTechLvl = (et != null) &&  
-                    (TechConstants.isLegal(getBattleArmor().getTechLevel(), 
-                            et.getTechLevel(getBattleArmor().getYear()), 
-                            isMixed));
-            if (legalTechLvl){
+            if ((null != et) && getTechManager().isLegal(et)) {
                 leftManipSelect.addItem(et.getName());
                 rightManipSelect.addItem(et.getName());
             }
@@ -331,28 +230,24 @@ public class StructureTab extends ITab implements ActionListener, ChangeListener
 	}
 
 	public void addAllListeners() {
-        armorType.addActionListener(this);
         leftManipSelect.addActionListener(this);
         rightManipSelect.addActionListener(this);
-        
-        armorPoints.addChangeListener(this);
         
         panBasicInfo.addListener(this);
         panChassis.addListener(this);
         panMovement.addListener(this);
+        panArmor.addListener(this);
         panEnhancements.addListener(this);
     }
 
     public void removeAllListeners() {
-        armorType.removeActionListener(this);
         leftManipSelect.removeActionListener(this);
         rightManipSelect.removeActionListener(this);
-        
-        armorPoints.removeChangeListener(this);
         
         panBasicInfo.removeListener(this);
         panChassis.removeListener(this);
         panMovement.removeListener(this);
+        panArmor.removeListener(this);
         panEnhancements.removeListener(this);
     }
 
@@ -367,31 +262,7 @@ public class StructureTab extends ITab implements ActionListener, ChangeListener
         if (e.getSource() instanceof JComboBox) {
             @SuppressWarnings("unchecked")
             JComboBox<String> combo = (JComboBox<String>) e.getSource();
-            if (combo.equals(armorType)) {
-                UnitUtil.removeISorArmorMounts(getBattleArmor(), false);
-                String armorName;
-                if (getBattleArmor().isMixedTech()){
-                    armorName = (String)armorType.getSelectedItem();
-                } else if (getBattleArmor().isClan()){
-                    armorName = "Clan " + (String)armorType.getSelectedItem();
-                } else {
-                    armorName = "IS " + (String)armorType.getSelectedItem();
-                }
-                EquipmentType aType = EquipmentType.get(armorName);
-                int armorCount = aType.getCriticals(getBattleArmor());
-                getBattleArmor().setArmorType(armorName);
-                getBattleArmor().setArmorTechLevel(
-                        aType.getTechLevel(getBattleArmor().getYear()));
-
-                for (; armorCount > 0; armorCount--) {
-                    try {
-                        getBattleArmor().addEquipment(new Mounted(getBattleArmor(), aType),
-                                BattleArmor.LOC_SQUAD, false);
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            } else if (combo.equals(leftManipSelect)){
+            if (combo.equals(leftManipSelect)){
                 // If the BA already had a manipulator here, we'll need to
                 //  remove it
                 Mounted leftManip = getBattleArmor().getLeftManipulator();
@@ -496,59 +367,21 @@ public class StructureTab extends ITab implements ActionListener, ChangeListener
         refresh.refreshAll();
     }
 
+    /**
+     * Extracts the actual name of the manipulator from the full equipment name
+     */
+    private String manipulatorDisplayName(String name) {
+        int start = name.indexOf("[");
+        if (start > 0) {
+            return name.substring(start + 1).replace("]", "");
+        }
+        return name;
+    }
+    
     public void setAsCustomization() {
         panBasicInfo.setAsCustomization();
     }
 
-    public void stateChanged(ChangeEvent e) {
-        removeAllListeners();
-
-        if (e.getSource() instanceof JSpinner) {
-            JSpinner spinner = (JSpinner) e.getSource();
-            if (spinner.equals(armorPoints)) {
-                int value = (Integer) armorPoints.getValue();
-                for(int i = 0; i < getBattleArmor().locations(); i++) {
-                    getBattleArmor().initializeArmor(value, i);
-                }
-            }
-        }
-
-        addAllListeners();
-
-        refresh.refreshAll();
-    }
-
-    public void resetMovementMinMax() {
-        if(getBattleArmor().getOriginalWalkMP() > getBattleArmor().getMaximumWalkMP()) {
-            getBattleArmor().setOriginalWalkMP(getBattleArmor().getMaximumWalkMP());
-        }
-        else if(getBattleArmor().getOriginalWalkMP() < getBattleArmor().getMinimumWalkMP()) {
-            getBattleArmor().setOriginalWalkMP(getBattleArmor().getMinimumWalkMP());
-        }
-        if(getBattleArmor().getOriginalJumpMP() > getBattleArmor().getMaximumJumpMP(true)) {
-            getBattleArmor().setOriginalJumpMP(getBattleArmor().getMaximumJumpMP(true));
-        }
-    }
-
-    public void resetArmorMax() {
-        for(int i = 0; i < getBattleArmor().locations(); i++) {
-            if(getBattleArmor().getOArmor(i) > getBattleArmor().getMaximumArmorPoints()) {
-                getBattleArmor().initializeArmor(getBattleArmor().getMaximumArmorPoints(), i);
-            }
-        }
-    }
-
-    public void resetJumpType() {
-        if(!getBattleArmor().isClan())  {
-            if(getBattleArmor().getOriginalJumpMP() > 0) {
-                getBattleArmor().setMovementMode(EntityMovementMode.INF_JUMP);
-            } else {
-                getBattleArmor().setMovementMode(EntityMovementMode.INF_LEG);
-            }
-            resetMovementMinMax();
-        }
-    }
-    
     public void refreshPreview(){
         boolean populateTextFields = true;
         MechView mechView = null;
@@ -626,7 +459,7 @@ public class StructureTab extends ITab implements ActionListener, ChangeListener
         panChassis.refresh();
         panMovement.refresh();
         panEnhancements.setFromEntity(getBattleArmor());
-//        panArmor.refresh();
+        panArmor.refresh();
         addAllListeners();
     }
 
@@ -758,6 +591,36 @@ public class StructureTab extends ITab implements ActionListener, ChangeListener
         panMovement.setFromEntity(getBattleArmor());
         refresh.refreshSummary();
         refresh.refreshBuild();
+        refresh.refreshPreview();
+    }
+
+    @Override
+    public void armorValueChanged(int points) {
+        for(int i = 0; i < getBattleArmor().locations(); i++) {
+            getBattleArmor().initializeArmor(points, i);
+        }
+        refresh.refreshStatus();
+        refresh.refreshPreview();
+    }
+
+    @Override
+    public void armorTypeChanged(EquipmentType armor) {
+        UnitUtil.removeISorArmorMounts(getBattleArmor(), false);
+        int armorCount = armor.getCriticals(getBattleArmor());
+        getBattleArmor().setArmorType(armor.getInternalName());
+        getBattleArmor().setArmorTechLevel(
+                armor.getTechLevel(getBattleArmor().getYear()));
+
+        for (; armorCount > 0; armorCount--) {
+            try {
+                getBattleArmor().addEquipment(new Mounted(getBattleArmor(), armor),
+                        BattleArmor.LOC_SQUAD, false);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+        refresh.refreshBuild();
+        refresh.refreshStatus();
         refresh.refreshPreview();
     }
 }
