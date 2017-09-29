b8a5094707380bcb768ca6bc70ff07fbdf25e8e7
Carl Spain <neoancient@megamek.org>
2017-09-29 14:43:53 -0500

    Show current slot count, basic limit, and weight for exceeding basic limit in each arc.

--- src/megameklab/com/ui/Dropship/views/DropshipCriticalView.java
+++ src/megameklab/com/ui/Dropship/views/DropshipCriticalView.java
@@ -14,14 +14,19 @@
 package megameklab.com.ui.Dropship.views;
 
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.ResourceBundle;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.TitledBorder;
 
-import megamek.common.SmallCraft;
+import megamek.common.util.EncodeControl;
+import megamek.common.verifier.TestSmallCraft;
 import megameklab.com.ui.EntitySource;
 import megameklab.com.ui.util.BayWeaponCriticalTree;
 import megameklab.com.util.IView;
@@ -43,12 +48,8 @@ public class DropshipCriticalView extends IView {
      */
     private static final long serialVersionUID = -3093586215625228103L;
     
-    private BayWeaponCriticalTree noseTree;
-    private BayWeaponCriticalTree leftTree;
-    private BayWeaponCriticalTree aftLeftTree;
-    private BayWeaponCriticalTree rightTree;
-    private BayWeaponCriticalTree aftRightTree;
-    private BayWeaponCriticalTree aftTree;
+    // Maximum number of arcs for small craft/dropship; aerodyne only use four
+    private static final int NUM_ARCS = 6;
     
     private JPanel nosePanel = new JPanel();
     private JPanel leftPanel = new JPanel();
@@ -60,15 +61,23 @@ public class DropshipCriticalView extends IView {
     private JPanel leftColumn = new JPanel();
     private JPanel midColumn = new JPanel();
     private JPanel rightColumn = new JPanel();
-    private RefreshListener refresh;
     
-    private boolean spheroid; // Track the chassis type so we can tell whether side arc equipment
+    private boolean spheroid = true; // Track the chassis type so we can tell whether side arc equipment
                               // needs to be shuffled when a refresh is triggered.
+    
+    private BayWeaponCriticalTree arcTrees[] = new BayWeaponCriticalTree[NUM_ARCS];
+    private String aerodyneArcNames[];
+    private String spheroidArcNames[];
+    private JLabel lblSlotCount[] = new JLabel[NUM_ARCS];
+    private JLabel lblExtraTonnage[] = new JLabel[NUM_ARCS];
+    
 
     public DropshipCriticalView(EntitySource eSource, RefreshListener refresh) {
         super(eSource);
-        this.refresh = refresh;
 
+        ResourceBundle resourceMap = ResourceBundle.getBundle("megameklab.resources.Views", new EncodeControl()); //$NON-NLS-1$
+        aerodyneArcNames = resourceMap.getString("DropshipCriticalView.aerodyneArcs.values").split(","); //$NON-NLS-1$
+        spheroidArcNames = resourceMap.getString("DropshipCriticalView.spheroidArcs.values").split(","); //$NON-NLS-1$
         JPanel mainPanel = new JPanel();
 
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
@@ -76,117 +85,136 @@ public class DropshipCriticalView extends IView {
         leftColumn.setLayout(new BoxLayout(leftColumn, BoxLayout.Y_AXIS));
         midColumn.setLayout(new BoxLayout(midColumn, BoxLayout.Y_AXIS));
         rightColumn.setLayout(new BoxLayout(rightColumn, BoxLayout.Y_AXIS));
-
-        noseTree = new BayWeaponCriticalTree(SmallCraft.LOC_NOSE, eSource, refresh);
-        leftTree = new BayWeaponCriticalTree(SmallCraft.LOC_LWING, eSource, refresh);
-        aftLeftTree = new BayWeaponCriticalTree(SmallCraft.LOC_LWING, eSource, refresh, BayWeaponCriticalTree.AFT);
-        rightTree = new BayWeaponCriticalTree(SmallCraft.LOC_RWING, eSource, refresh);
-        aftRightTree = new BayWeaponCriticalTree(SmallCraft.LOC_RWING, eSource, refresh, BayWeaponCriticalTree.AFT);
-        aftTree = new BayWeaponCriticalTree(SmallCraft.LOC_AFT, eSource, refresh);
+        
+        for (int arc = 0; arc < NUM_ARCS; arc++) {
+            if (arc < getSmallCraft().locations()) {
+                arcTrees[arc] = new BayWeaponCriticalTree(arc, eSource, refresh);
+            } else {
+                arcTrees[arc] = new BayWeaponCriticalTree(arc - 3, eSource, refresh, BayWeaponCriticalTree.AFT);
+            }
+        }
 
         leftColumn.add(Box.createVerticalGlue());
-        leftPanel.add(leftTree);
-        leftTree.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, Color.black));
-        leftPanel.setBorder(BorderFactory.createTitledBorder(
-                BorderFactory.createEmptyBorder(), "Left Side",
-                TitledBorder.TOP, TitledBorder.DEFAULT_POSITION));
+        leftPanel = createArcPanel(TestSmallCraft.ARC_FWD_LEFT, resourceMap);
         leftColumn.add(leftPanel);
-
-        aftLeftPanel.add(aftLeftTree);
-        aftLeftTree.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, Color.black));
-        aftLeftPanel.setBorder(BorderFactory.createTitledBorder(
-                BorderFactory.createEmptyBorder(), "Aft Left",
-                TitledBorder.TOP, TitledBorder.DEFAULT_POSITION));
+        aftLeftPanel = createArcPanel(TestSmallCraft.ARC_AFT_LEFT, resourceMap);
         leftColumn.add(aftLeftPanel);
+        leftColumn.add(Box.createVerticalGlue());
 
-        nosePanel.add(noseTree);
-        noseTree.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, Color.black));
-        nosePanel.setBorder(BorderFactory.createTitledBorder(
-                BorderFactory.createEmptyBorder(), "Nose",
-                TitledBorder.TOP, TitledBorder.DEFAULT_POSITION));
+        nosePanel = createArcPanel(TestSmallCraft.ARC_NOSE, resourceMap);
         midColumn.add(nosePanel);
-
         midColumn.add(Box.createVerticalGlue());
-        aftPanel.add(aftTree);
-        aftTree.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, Color.black));
-        aftPanel.setBorder(BorderFactory.createTitledBorder(
-                BorderFactory.createEmptyBorder(), "Aft",
-                TitledBorder.TOP, TitledBorder.DEFAULT_POSITION));
+        aftPanel = createArcPanel(TestSmallCraft.ARC_AFT, resourceMap);
         midColumn.add(aftPanel);
-
+        
         rightColumn.add(Box.createVerticalGlue());
-        rightPanel.add(rightTree);
-        rightTree.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, Color.black));
-        rightPanel.setBorder(BorderFactory.createTitledBorder(
-                BorderFactory.createEmptyBorder(), "Right Side",
-                TitledBorder.TOP, TitledBorder.DEFAULT_POSITION));
+        rightPanel = createArcPanel(TestSmallCraft.ARC_FWD_RIGHT, resourceMap);
         rightColumn.add(rightPanel);
-
-        aftRightPanel.add(aftRightTree);
-        aftRightTree.setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, Color.black));
-        aftRightPanel.setBorder(BorderFactory.createTitledBorder(
-                BorderFactory.createEmptyBorder(), "Aft Right",
-                TitledBorder.TOP, TitledBorder.DEFAULT_POSITION));
+        aftRightPanel = createArcPanel(TestSmallCraft.ARC_AFT_RIGHT, resourceMap);
         rightColumn.add(aftRightPanel);
+        rightColumn.add(Box.createVerticalGlue());
 
         mainPanel.add(leftColumn);
         mainPanel.add(midColumn);
         mainPanel.add(rightColumn);
-        this.add(mainPanel);
+        add(mainPanel);
         
         refresh();
 
     }
     
+    private JPanel createArcPanel(int arc, ResourceBundle resourceMap) {
+        JPanel arcPanel = new JPanel(new GridBagLayout());
+        arcTrees[arc].setBorder(BorderFactory.createMatteBorder(0, 1, 1, 1, Color.black));
+        arcPanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEmptyBorder(), spheroidArcNames[arc],
+                TitledBorder.TOP, TitledBorder.DEFAULT_POSITION));
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        arcPanel.add(arcTrees[arc], gbc);
+        
+        lblSlotCount[arc] = new JLabel();
+        JLabel lbl = new JLabel(resourceMap.getString("DropshipCriticalView.lblSlotCount.text")); //$NON-NLS-1$
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridwidth = 1;
+        gbc.fill = GridBagConstraints.NONE;
+        arcPanel.add(lbl, gbc);
+        gbc.gridx = 1;
+        arcPanel.add(lblSlotCount[arc], gbc);
+        lblSlotCount[arc].setToolTipText(resourceMap.getString("DropshipCriticalView.lblSlotCount.tooltip")); //$NON-NLS-1$
+        
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridwidth = 2;
+        lbl = new JLabel(String.format(resourceMap.getString("DropshipCriticalView.lblMaxSlots.format"), //$NON-NLS-1$
+                TestSmallCraft.SLOTS_PER_ARC));
+        lbl.setToolTipText(resourceMap.getString("DropshipCriticalView.lblMaxSlots.format")); //$NON-NLS-1$
+        arcPanel.add(lbl, gbc);
+        
+        gbc.gridx = 0;
+        gbc.gridy++;
+        gbc.gridwidth = 1;
+        lbl = new JLabel(resourceMap.getString("DropshipCriticalView.lblExtraWeight.text")); //$NON-NLS-1$
+        arcPanel.add(lbl, gbc);
+        lblExtraTonnage[arc] = new JLabel();
+        gbc.gridx = 1;
+        arcPanel.add(lblExtraTonnage[arc], gbc);
+        
+        return arcPanel;
+    }
+    
     public void addAllocationListeners(AerospaceBuildView abv) {
-        abv.addArcView(noseTree);
-        abv.addArcView(leftTree);
-        abv.addArcView(rightTree);
-        abv.addArcView(aftLeftTree);
-        abv.addArcView(aftRightTree);
-        abv.addArcView(aftTree);
+        for (BayWeaponCriticalTree tree : arcTrees) {
+            abv.addArcView(tree);
+        }
     }
 
     public void updateRefresh(RefreshListener refresh) {
-        this.refresh = refresh;
-        noseTree.updateRefresh(refresh);
-        leftTree.updateRefresh(refresh);
-        rightTree.updateRefresh(refresh);
-        aftTree.updateRefresh(refresh);
-        aftLeftTree.updateRefresh(refresh);
-        aftRightTree.updateRefresh(refresh);
+        for (BayWeaponCriticalTree tree : arcTrees) {
+            tree.updateRefresh(refresh);
+        }
     }
 
     public void refresh() {
         if (getSmallCraft().isSpheroid()) {
-            ((TitledBorder)leftPanel.getBorder()).setTitle("Fore Left");
-            ((TitledBorder)rightPanel.getBorder()).setTitle("Fore Right");
-            leftTree.setFacing(BayWeaponCriticalTree.FORWARD);
-            rightTree.setFacing(BayWeaponCriticalTree.FORWARD);
+            ((TitledBorder)leftPanel.getBorder()).setTitle(spheroidArcNames[TestSmallCraft.ARC_FWD_LEFT]);
+            ((TitledBorder)rightPanel.getBorder()).setTitle(spheroidArcNames[TestSmallCraft.ARC_FWD_RIGHT]);
+            arcTrees[TestSmallCraft.ARC_FWD_LEFT].setFacing(BayWeaponCriticalTree.FORWARD);
+            arcTrees[TestSmallCraft.ARC_FWD_RIGHT].setFacing(BayWeaponCriticalTree.FORWARD);
             aftLeftPanel.setVisible(true);
             aftRightPanel.setVisible(true);
         } else {
-            ((TitledBorder)leftPanel.getBorder()).setTitle("Left Wing");
-            ((TitledBorder)rightPanel.getBorder()).setTitle("Right Wing");
-            leftTree.setFacing(BayWeaponCriticalTree.BOTH);
-            rightTree.setFacing(BayWeaponCriticalTree.BOTH);
+            ((TitledBorder)leftPanel.getBorder()).setTitle(aerodyneArcNames[TestSmallCraft.ARC_LWING]);
+            ((TitledBorder)rightPanel.getBorder()).setTitle(aerodyneArcNames[TestSmallCraft.ARC_RWING]);
+            arcTrees[TestSmallCraft.ARC_LWING].setFacing(BayWeaponCriticalTree.BOTH);
+            arcTrees[TestSmallCraft.ARC_RWING].setFacing(BayWeaponCriticalTree.BOTH);
             aftLeftPanel.setVisible(false);
             aftRightPanel.setVisible(false);
         }
 
         if (spheroid != getSmallCraft().isSpheroid()) {
-            leftTree.rebuild();
-            rightTree.rebuild();
-            aftLeftTree.rebuild();
-            aftRightTree.rebuild();
+            arcTrees[TestSmallCraft.ARC_FWD_LEFT].rebuild();
+            arcTrees[TestSmallCraft.ARC_FWD_RIGHT].rebuild();
+            arcTrees[TestSmallCraft.ARC_AFT_LEFT].rebuild();
+            arcTrees[TestSmallCraft.ARC_AFT_RIGHT].rebuild();
             spheroid = getSmallCraft().isSpheroid();
         } else {
-            noseTree.repaint();
-            leftTree.repaint();
-            rightTree.repaint();
-            aftLeftTree.repaint();
-            aftRightTree.repaint();
-            aftTree.repaint();
+            for (BayWeaponCriticalTree tree : arcTrees) {
+                tree.repaint();
+            }
         }
+        
+        double[] extra = TestSmallCraft.extraSlotCost(getSmallCraft());
+        for (int arc = 0; arc < extra.length; arc++) {
+            lblSlotCount[arc].setText(String.valueOf(arcTrees[arc].getSlotCount()));
+            lblExtraTonnage[arc].setText(String.valueOf(extra[arc]));
+        }
+        
     }
 }
