555253135e44f9a66b1c2eab86b652a8f36f739b
Carl Spain <neoancient@megamek.org>
2017-10-07 21:06:27 -0500

    Added spinner as cell editor for bay size and doors.

--- src/megameklab/com/ui/Dropship/tabs/TransportTab.java
+++ src/megameklab/com/ui/Dropship/tabs/TransportTab.java
@@ -13,16 +13,25 @@
  */
 package megameklab.com.ui.Dropship.tabs;
 
+import java.awt.Component;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.AbstractCellEditor;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JScrollPane;
+import javax.swing.JSpinner;
 import javax.swing.JTable;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellEditor;
+import javax.swing.table.TableColumn;
 
 import megamek.common.Bay;
+import megamek.common.Entity;
 import megamek.common.InfantryBay;
 import megamek.common.verifier.TestAero;
 import megameklab.com.ui.EntitySource;
@@ -35,6 +44,11 @@ import megameklab.com.util.IView;
  *
  */
 public class TransportTab extends IView {
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6288658666144030993L;
     
     private final InstalledBaysModel modelInstalled = new InstalledBaysModel();
     private final JTable tblInstalled = new JTable(modelInstalled);
@@ -50,15 +64,34 @@ public class TransportTab extends IView {
         add(new JScrollPane(tblAvailable));
         modelInstalled.refreshBays();
         modelAvailable.refreshBays();
+        
+        tblInstalled.setRowHeight(24);
+        TableColumn col = tblInstalled.getColumnModel().getColumn(InstalledBaysModel.COL_SIZE);
+        col.setCellEditor(new SpinnerCellEditor(InstalledBaysModel.COL_SIZE));
+        col = tblInstalled.getColumnModel().getColumn(InstalledBaysModel.COL_DOORS);
+        col.setCellEditor(new SpinnerCellEditor(InstalledBaysModel.COL_DOORS));
+    }
+    
+    private int doorsAvailable() {
+        int total = TestAero.maxBayDoors(getAero());
+        for (Bay bay : getAero().getTransportBays()) {
+            total -= bay.getDoors();
+        }
+        return Math.max(total, 0);
     }
     
     private class InstalledBaysModel extends AbstractTableModel {
         
+        /**
+         * 
+         */
+        private static final long serialVersionUID = -8643492032818089043L;
+        
         private static final int COL_NAME      = 0;
         private static final int COL_SIZE      = 1;
         private static final int COL_DOORS     = 2;
-        private static final int COL_PERSONNEL = 3;
-        private static final int COL_TONNAGE   = 4;
+        private static final int COL_TONNAGE   = 3;
+        private static final int COL_PERSONNEL = 4;
         private static final int NUM_COLS      = 5;
         
         private final List<Bay> bayList = new ArrayList<>();
@@ -107,25 +140,42 @@ public class TransportTab extends IView {
                 case COL_NAME:
                     return bayTypeList.get(rowIndex).getDisplayName();
                 case COL_SIZE:
+                    if (bayTypeList.get(rowIndex) != TestAero.TransportBay.CARGO) {
+                        return (int)bayList.get(rowIndex).getUnusedSlots();
+                    }
                     return bayList.get(rowIndex).getUnusedSlots();
                 case COL_DOORS:
                     return bayList.get(rowIndex).getDoors();
-                case COL_TONNAGE:
-                    return bayList.get(rowIndex).getWeight();
                 case COL_PERSONNEL:
                     if (bayList.get(rowIndex) instanceof InfantryBay) {
                         return "*";
                     }
                     return bayTypeList.get(rowIndex).getPersonnel()
                             * (int)bayList.get(rowIndex).getCapacity();
+                case COL_TONNAGE:
+                    return bayList.get(rowIndex).getWeight();
                 default:
                     return null;
             }
         }
 
+        @Override
+        public boolean isCellEditable(int rowIndex, int columnIndex) {
+            if (columnIndex == COL_DOORS) {
+                return getAero().hasETypeFlag(Entity.ETYPE_DROPSHIP)
+                        || getAero().hasETypeFlag(Entity.ETYPE_JUMPSHIP);
+            }
+            return (columnIndex == COL_SIZE);
+        }
+
     }
     
     private class AvailableBaysModel extends AbstractTableModel {
+        
+        /**
+         * 
+         */
+        private static final long serialVersionUID = -5456813671712646392L;
         
         private static final int COL_NAME      = 0;
         private static final int COL_SIZE      = 1;
@@ -183,6 +233,72 @@ public class TransportTab extends IView {
                 }
             }            
             return "?";
+        }
+        
+    }
+    
+    private class SpinnerCellEditor extends AbstractCellEditor implements TableCellEditor, ChangeListener {
+        
+        /**
+         * 
+         */
+        private static final long serialVersionUID = -5334192308060664513L;
+        
+        private final JSpinner spinner = new JSpinner();
+        private final int column;
+        
+        SpinnerCellEditor(int column) {
+            this.column = column;
+            spinner.addChangeListener(this);
+        }
+
+        @Override
+        public Object getCellEditorValue() {
+            return spinner.getValue();
+        }
+
+        @Override
+        public void stateChanged(ChangeEvent e) {
+            int row = tblInstalled.convertRowIndexToModel(tblInstalled.getSelectedRow());
+            if ((row < 0) || (row >= modelInstalled.bayList.size())) {
+                return;
+            }
+            final Bay bay = modelInstalled.bayList.get(row);
+            if (column == InstalledBaysModel.COL_SIZE) {
+                Bay newBay = modelInstalled.bayTypeList.get(row).newBay((Double)getCellEditorValue(),
+                        bay.getBayNumber());
+                newBay.setDoors(bay.getDoors());
+                getAero().removeTransporter(bay);
+                getAero().addTransporter(newBay);
+                modelInstalled.bayList.set(row, newBay);
+            } else if (column == InstalledBaysModel.COL_DOORS) {
+                modelInstalled.bayList.get(row).setDoors((Integer)getCellEditorValue());
+            }
+            modelInstalled.fireTableRowsUpdated(row, row);
+        }
+
+        @Override
+        public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row,
+                int column) {
+            boolean isCargo = modelInstalled.bayTypeList.get(row) == TestAero.TransportBay.CARGO;
+            if (column == InstalledBaysModel.COL_DOORS) {
+                int doors = ((Integer)modelInstalled.getValueAt(row,  column)).intValue();
+                SpinnerNumberModel model = new SpinnerNumberModel(doors,
+                        isCargo? 0 : 1, doorsAvailable() + doors, 1);
+                spinner.removeChangeListener(this);
+                spinner.setModel(model);
+                spinner.addChangeListener(this);
+                return spinner;
+            } else if (column == InstalledBaysModel.COL_SIZE) {
+                double step = isCargo? 0.5 : 1.0;
+                SpinnerNumberModel model = new SpinnerNumberModel(modelInstalled.bayList.get(row).getUnusedSlots(),
+                            step, null, step);
+                spinner.removeChangeListener(this);
+                spinner.setModel(model);
+                spinner.addChangeListener(this);
+                return spinner;
+            }
+            return null;
         }
         
     }
