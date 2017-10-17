1744e3f498c0436757c601e8767381f6933677f4
Carl Spain <neoancient@megamek.org>
2017-10-17 10:20:17 -0500

    DnD support for allocating multiple pieces of equipment to a location in single action.

--- src/megameklab/com/ui/util/AeroBayTransferHandler.java
+++ src/megameklab/com/ui/util/AeroBayTransferHandler.java
@@ -21,6 +21,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.StringJoiner;
 
 import javax.swing.JComponent;
 import javax.swing.JTable;
@@ -73,32 +74,35 @@ public class AeroBayTransferHandler extends TransferHandler {
         
         // Fields are equipmentNum, node child index, bay child index
         String[] source = null;
-        int eqNum = -1;
+        List<Mounted> eqList = new ArrayList<>();
         
         try {
-            source = ((String) support.getTransferable().getTransferData(DataFlavor.stringFlavor)).split(",");
-            eqNum = Integer.parseInt(source[0]);
+            source = ((String) support.getTransferable().getTransferData(DataFlavor.stringFlavor)).split(":");
+            for (String field : source[0].split(",")) {
+                int eqNum = Integer.parseInt(field);
+                Mounted m = eSource.getEntity().getEquipment(eqNum);
+                if (null != m) {
+                    eqList.add(m);
+                }
+            }
         } catch (Exception ex) {
             MegaMekLab.getLogger().log(AeroBayTransferHandler.class,
                     "importData(TransferSupport)", //$NON-NLS-1$
                     ex);
             return false;
         }
-        if (eqNum < 0) {
-            return false;
-        }
-        final Mounted mount = eSource.getEntity().getEquipment(eqNum);
-        if (null == mount) {
+        if (eqList.isEmpty()) {
             return false;
         }
 
         if ((support.getComponent() instanceof BayWeaponCriticalTree)) {
             final BayWeaponCriticalTree tree = (BayWeaponCriticalTree) support.getComponent();
-            if (eSource.getEntity().usesWeaponBays()) {
+            if (eSource.getEntity().usesWeaponBays() && (eqList.size() == 1)) {
                 // If it's a bay we move it and its entire contents. Otherwise we find the bay that was
                 // dropped on and add it there. A weapon dropped on an illegal bay will create a new one
                 // and non-bay equipment will be added at the top level regardless of the drop location.
                 // Non-weapon bay equipment cannot be dropped on an illegal bay.
+                final Mounted mount = eqList.get(0);
                 if (mount.getType() instanceof BayWeapon) {
                     tree.addBay(mount);
                 } else if ((mount.getType() instanceof AmmoType) && (support.getUserDropAction() == AMMO_SINGLE)) {
@@ -111,67 +115,69 @@ public class AeroBayTransferHandler extends TransferHandler {
                 }
             } else {
                 // Small craft don't use bays.
-                tree.addToLocation(mount);
+                tree.addToLocation(eqList);
             }
         } else {
             // Target is unallocated bay table.
-            if (mount.getType() instanceof AmmoType) {
-                AmmoType at = (AmmoType)mount.getType();
-                // Check whether we are moving one of multiple slots. 
-                if ((support.getUserDropAction() == AMMO_SINGLE) && (mount.getUsableShotsLeft() > at.getShots())) {
-                    mount.setShotsLeft(mount.getUsableShotsLeft() - at.getShots());
-                }
-                Mounted addMount = UnitUtil.findUnallocatedAmmo(eSource.getEntity(), at);
-                if (null != addMount) {
-                    if (support.getUserDropAction() == AMMO_SINGLE) {
-                        addMount.setShotsLeft(addMount.getUsableShotsLeft() + at.getShots());
+            for (Mounted mount : eqList) {
+                if (mount.getType() instanceof AmmoType) {
+                    AmmoType at = (AmmoType)mount.getType();
+                    // Check whether we are moving one of multiple slots. 
+                    if ((support.getUserDropAction() == AMMO_SINGLE) && (mount.getUsableShotsLeft() > at.getShots())) {
+                        mount.setShotsLeft(mount.getUsableShotsLeft() - at.getShots());
+                    }
+                    Mounted addMount = UnitUtil.findUnallocatedAmmo(eSource.getEntity(), at);
+                    if (null != addMount) {
+                        if (support.getUserDropAction() == AMMO_SINGLE) {
+                            addMount.setShotsLeft(addMount.getUsableShotsLeft() + at.getShots());
+                        } else {
+                            addMount.setShotsLeft(addMount.getUsableShotsLeft() + mount.getUsableShotsLeft());
+                        }
                     } else {
-                        addMount.setShotsLeft(addMount.getUsableShotsLeft() + mount.getUsableShotsLeft());
-                    }
-                } else {
-                    try {
-                        Mounted m = eSource.getEntity().addEquipment(at, Entity.LOC_NONE);
-                        if (support.getUserDropAction() == AMMO_ALL) {
-                            m.setShotsLeft(mount.getUsableShotsLeft());
-                        }
-                    } catch (LocationFullException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-            } else {
-                List<Mounted> toRemove;
-                if (mount.getType() instanceof BayWeapon) {
-                    toRemove = new ArrayList<>();
-                    for (Integer num : mount.getBayWeapons()) {
-                        toRemove.add(eSource.getEntity().getEquipment(num));
-                    }
-                    for (Integer num : mount.getBayAmmo()) {
-                        toRemove.add(eSource.getEntity().getEquipment(num));
-                    }
-                } else {
-                    toRemove = Collections.singletonList(mount);
-                }
-                for (Mounted m : toRemove) {
-                    if (m.getType() instanceof AmmoType) {
-                        Mounted aMount = UnitUtil.findUnallocatedAmmo(eSource.getEntity(), m.getType());
-                        if (null != aMount) {
-                            aMount.setShotsLeft(aMount.getUsableShotsLeft() + m.getUsableShotsLeft());
-                            m.setShotsLeft(0);
-                            continue;
+                        try {
+                            Mounted m = eSource.getEntity().addEquipment(at, Entity.LOC_NONE);
+                            if (support.getUserDropAction() == AMMO_ALL) {
+                                m.setShotsLeft(mount.getUsableShotsLeft());
+                            }
+                        } catch (LocationFullException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
                         }
                     }
-                    UnitUtil.removeCriticals(eSource.getEntity(), m);
-                    UnitUtil.changeMountStatus(eSource.getEntity(), m, Entity.LOC_NONE, Entity.LOC_NONE, false);
-                    if ((mount.getType() instanceof WeaponType) && (m.getLinkedBy() != null)) {
-                        UnitUtil.removeCriticals(eSource.getEntity(), m.getLinkedBy());
-                        UnitUtil.changeMountStatus(eSource.getEntity(), m.getLinkedBy(),
-                                Entity.LOC_NONE, Entity.LOC_NONE, false);
-                        m.getLinkedBy().setLinked(null);
-                        m.setLinkedBy(null);
+                } else {
+                    List<Mounted> toRemove;
+                    if (mount.getType() instanceof BayWeapon) {
+                        toRemove = new ArrayList<>();
+                        for (Integer num : mount.getBayWeapons()) {
+                            toRemove.add(eSource.getEntity().getEquipment(num));
+                        }
+                        for (Integer num : mount.getBayAmmo()) {
+                            toRemove.add(eSource.getEntity().getEquipment(num));
+                        }
+                    } else {
+                        toRemove = Collections.singletonList(mount);
                     }
+                    for (Mounted m : toRemove) {
+                        if (m.getType() instanceof AmmoType) {
+                            Mounted aMount = UnitUtil.findUnallocatedAmmo(eSource.getEntity(), m.getType());
+                            if (null != aMount) {
+                                aMount.setShotsLeft(aMount.getUsableShotsLeft() + m.getUsableShotsLeft());
+                                m.setShotsLeft(0);
+                                continue;
+                            }
+                        }
+                        UnitUtil.removeCriticals(eSource.getEntity(), m);
+                        UnitUtil.changeMountStatus(eSource.getEntity(), m, Entity.LOC_NONE, Entity.LOC_NONE, false);
+                        if ((mount.getType() instanceof WeaponType) && (m.getLinkedBy() != null)) {
+                            UnitUtil.removeCriticals(eSource.getEntity(), m.getLinkedBy());
+                            UnitUtil.changeMountStatus(eSource.getEntity(), m.getLinkedBy(),
+                                    Entity.LOC_NONE, Entity.LOC_NONE, false);
+                            m.getLinkedBy().setLinked(null);
+                            m.setLinkedBy(null);
+                        }
+                    }
+                    UnitUtil.compactCriticals(eSource.getEntity());
                 }
-                UnitUtil.compactCriticals(eSource.getEntity());
             }
         }
         return true;
@@ -184,13 +190,15 @@ public class AeroBayTransferHandler extends TransferHandler {
             return false;
         }
         // check if the dragged mounted should be transferrable
-        Mounted mounted = null;
+        List<Mounted> mounted = new ArrayList<>();
         try {
             String str = (String) support.getTransferable().getTransferData(DataFlavor.stringFlavor);
-            if (str.contains(",")) {
-                str = str.substring(0, str.indexOf(","));
+            if (str.contains(":")) {
+                str = str.substring(0, str.indexOf(":"));
             }
-            mounted = eSource.getEntity().getEquipment(Integer.parseInt(str));
+            for (String field : str.split(",")) {
+                mounted.add(eSource.getEntity().getEquipment(Integer.parseInt(field)));
+            }
         } catch (NumberFormatException e) {
             e.printStackTrace();
         } catch (UnsupportedFlavorException e) {
@@ -199,14 +207,19 @@ public class AeroBayTransferHandler extends TransferHandler {
             e.printStackTrace();
         }
         // not actually dragged a Mounted? not transferable
-        if (mounted == null) {
+        if (mounted.isEmpty()) {
             return false;
         }
         
         // If allocating to an arc, make sure the bay can receive it
         if (support.getComponent() instanceof BayWeaponCriticalTree) {
-            return ((BayWeaponCriticalTree)support.getComponent())
-                    .isValidDropLocation((JTree.DropLocation)support.getDropLocation(), mounted);
+            for (Mounted m : mounted) {
+                if (((BayWeaponCriticalTree)support.getComponent())
+                        .isValidDropLocation((JTree.DropLocation)support.getDropLocation(), m)) {
+                    return true;
+                }
+            }
+            return false;
         }
         return true;
     }
@@ -217,8 +230,12 @@ public class AeroBayTransferHandler extends TransferHandler {
             return new StringSelection(((BayWeaponCriticalTree)c).encodeSelection());
         } else {
             JTable table = (JTable) c;
-            Mounted mount = (Mounted) ((CriticalTableModel) table.getModel()).getValueAt(table.getSelectedRow(), CriticalTableModel.EQUIPMENT);
-            return new StringSelection(Integer.toString(eSource.getEntity().getEquipmentNum(mount)));
+            StringJoiner sj = new StringJoiner(",");
+            for (int row : table.getSelectedRows()) {
+                Mounted mount = (Mounted) ((CriticalTableModel) table.getModel()).getValueAt(row, CriticalTableModel.EQUIPMENT);
+                sj.add(Integer.toString(eSource.getEntity().getEquipmentNum(mount)));
+            }
+            return new StringSelection(sj.toString());
         }
     }
 
