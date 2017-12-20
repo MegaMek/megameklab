d2310ab7df7153242312eb34da3b8a43534cbc43
Carl Spain <neoancient@megamek.org>
2017-12-19 21:37:47 -0600

    Reinitialize threshold in aerospace units when the armor value changes.
    Fixes #86: Aerospace Asset Armor Threshold

--- src/megameklab/com/ui/Aero/tabs/StructureTab.java
+++ src/megameklab/com/ui/Aero/tabs/StructureTab.java
@@ -558,6 +558,7 @@ public class StructureTab extends ITab implements AeroBuildListener {
     @Override
     public void armorPointsChanged(int location, int front, int rear) {
         getAero().initializeArmor(front, location);
+        getAero().initializeThresh(location);
         if (panArmor.getArmorType() == EquipmentType.T_ARMOR_PATCHWORK) {
             getAero().setArmorTonnage(panArmorAllocation.getTotalArmorWeight(getAero()));
         }
@@ -597,6 +598,7 @@ public class StructureTab extends ITab implements AeroBuildListener {
         getAero().initializeArmor(wing, Aero.LOC_LWING);
         getAero().initializeArmor(wing, Aero.LOC_RWING);
         getAero().initializeArmor(aft, Aero.LOC_AFT);
+        getAero().autoSetThresh();
 
         panArmorAllocation.setFromEntity(getAero());
         refresh.refreshPreview();
