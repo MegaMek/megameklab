02b7bfc1e2deef324e28096f47fad08a4913dc2c
Dylan Myers <dylan.myers@bluemedora.com>
2018-03-13 11:12:51 -0400

    Ready to develop <0.43.10>

--- src/megameklab/com/MegaMekLab.java
+++ src/megameklab/com/MegaMekLab.java
@@ -32,7 +32,7 @@ import megamek.common.logging.MMLogger;
 import megameklab.com.ui.Mek.MainUI;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.43.9-RC3";
+    public static final String VERSION = "0.43.10-git";
 
     private static MMLogger logger = null;
 
