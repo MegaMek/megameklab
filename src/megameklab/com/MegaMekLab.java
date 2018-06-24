9fe0f1b4601fc2a63cbb3934ed199aafac3cf575
HammerGS <drivingguy@hotmail.com>
2018-06-24 14:03:26 -0600

    Ready to Develop <v0.45.0>

--- src/megameklab/com/MegaMekLab.java
+++ src/megameklab/com/MegaMekLab.java
@@ -32,7 +32,7 @@ import megamek.common.logging.MMLogger;
 import megameklab.com.ui.Mek.MainUI;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.44.0";
+    public static final String VERSION = "0.45.0-git";
 
     private static MMLogger logger = null;
 
