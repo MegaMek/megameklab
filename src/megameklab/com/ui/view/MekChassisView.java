78c77b4640cb9684898ebd55d1e839cb33c6b36e
Carl Spain <neoancient@megamek.org>
2018-07-23 10:55:29 -0500

    Added debugging code for intermittent issue with no legal mech cockpit.

--- src/megameklab/com/ui/view/MekChassisView.java
+++ src/megameklab/com/ui/view/MekChassisView.java
@@ -45,7 +45,9 @@ import megamek.common.Mounted;
 import megamek.common.QuadVee;
 import megamek.common.SimpleTechLevel;
 import megamek.common.TechConstants;
+import megamek.common.logging.LogLevel;
 import megamek.common.util.EncodeControl;
+import megameklab.com.MegaMekLab;
 import megameklab.com.ui.util.CustomComboBox;
 import megameklab.com.ui.util.TechComboBox;
 import megameklab.com.ui.view.listeners.MekBuildListener;
@@ -501,6 +503,8 @@ public class MekChassisView extends BuildView implements ActionListener, ChangeL
     }
     
     private void refreshCockpit() {
+        final String METHOD_NAME = "refreshCockpit()";
+        
         cbCockpit.removeActionListener(this);
         Integer prev = (Integer)cbCockpit.getSelectedItem();
         cbCockpit.removeAllItems();
@@ -530,7 +534,17 @@ public class MekChassisView extends BuildView implements ActionListener, ChangeL
         cbCockpit.setSelectedItem(prev);
         cbCockpit.addActionListener(this);
         if (cbCockpit.getSelectedIndex() < 0) {
-            cbCockpit.setSelectedIndex(0);
+            try {
+                cbCockpit.setSelectedIndex(0);
+            } catch (NullPointerException ex) {
+                MegaMekLab.getLogger().log(getClass(), METHOD_NAME, LogLevel.ERROR,
+                        "No legal cockpit found"
+                                + "\nBase type: " + getBaseTypeIndex()
+                                + "\nTonnage: " + getTonnage()
+                                + "\nTech Year: " + techManager.getGameYear()
+                                + "\nTech Level: " + techManager.getTechLevel()
+                                + "\nTech Intro Year: " + techManager.getTechIntroYear());
+            }
         }
     }
     
