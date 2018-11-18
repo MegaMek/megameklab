878fc20277f3f2a638f402478aa05c429e5854e5
HammerGS <driving@hotmail.com>
2018-11-17 19:13:35 -0700

    Development Snapshot <v0.45.2>

--- src/megameklab/com/MegaMekLab.java
+++ src/megameklab/com/MegaMekLab.java
@@ -35,7 +35,7 @@ import megamek.common.preference.PreferenceManager;
 import megameklab.com.ui.Mek.MainUI;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.45.2-SNAPSHOT";
+    public static final String VERSION = "0.45.2";
 
     private static MMLogger logger = null;
 
