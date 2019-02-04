04b54f75e8a11a2441870b631a24704a0bb8a91c
Carl Spain <neoancient@megamek.org>
2019-02-04 16:25:08 -0600

    Development snapshot <v0.45.3>

--- src/megameklab/com/MegaMekLab.java
+++ src/megameklab/com/MegaMekLab.java
@@ -35,7 +35,7 @@ import megamek.common.preference.PreferenceManager;
 import megameklab.com.ui.Mek.MainUI;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.45.3-SNAPSHOT";
+    public static final String VERSION = "0.45.3";
 
     private static MMLogger logger = null;
 
