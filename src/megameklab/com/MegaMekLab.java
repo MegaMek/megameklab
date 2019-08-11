95fdabc3507a65c36a266a62c25ed830f4671f1f
Carl Spain <neoancient@megamek.org>
2019-08-11 18:48:58 -0500

    Release candidate 0.46.0-rc.2

--- src/megameklab/com/MegaMekLab.java
+++ src/megameklab/com/MegaMekLab.java
@@ -35,7 +35,7 @@ import megamek.common.preference.PreferenceManager;
 import megameklab.com.ui.Mek.MainUI;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.46.0-SNAPSHOT";
+    public static final String VERSION = "0.46.0-rc.2";
 
     private static MMLogger logger = null;
 
