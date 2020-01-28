632bf78bdb2b0b2a813b03af56b20058e2e91b8e
Carl Spain <neoancient@megamek.org>
2020-01-27 23:20:41 -0600

    Added javadoc comments to InventoryEntry interface

--- src/megameklab/com/printing/InventoryEntry.java
+++ src/megameklab/com/printing/InventoryEntry.java
@@ -24,27 +24,74 @@ package megameklab.com.printing;
 public interface InventoryEntry {
     String DASH = "\u2014";
 
+    /**
+     * @return The number of rows this entry takes on the table. This does not consider text that is
+     *         wrapped due to insufficient field width.
+     */
     int nRows();
 
+    /**
+     * @param row The row index within the entry. Should be < nRows()
+     * @return    The number of identical equipment pieces represented by this entry
+     */
     String getQuantityField(int row);
 
+    /**
+     * @param row The row index within the entry. Should be < nRows()
+     * @return    The name of the equipment
+     */
     String getNameField(int row);
 
+    /**
+     * @param row The row index within the entry. Should be < nRows()
+     * @return    The location/firing arc
+     */
     String getLocationField(int row);
 
+    /**
+     * @param row The row index within the entry. Should be < nRows()
+     * @return    The amount of heat generated
+     */
     String getHeatField(int row);
 
+    /**
+     * @param row The row index within the entry. Should be < nRows()
+     * @return    The damage caused by the weapon, as well as its type code
+     */
     String getDamageField(int row);
 
+    /**
+     * @param row The row index within the entry. Should be < nRows()
+     * @return    The upper limit of the minimum range
+     */
     String getMinField(int row);
 
+    /**
+     * @param row The row index within the entry. Should be < nRows()
+     * @return    The upper limit of the short range (ground units) or SRV (aero units)
+     */
     String getShortField(int row);
 
+    /**
+     * @param row The row index within the entry. Should be < nRows()
+     * @return    The upper limit of the medium range (ground units) or MRV (aero units)
+     */
     String getMediumField(int row);
 
+    /**
+     * @param row The row index within the entry. Should be < nRows()
+     * @return    The upper limit of the long range (ground units) or LRV (aero units)
+     */
     String getLongField(int row);
 
+    /**
+     * @param row The row index within the entry. Should be < nRows()
+     * @return    The ERV (aero units)
+     */
     String getExtremeField(int row);
 
+    /**
+     * @return Whether lines after the first line should be indented
+     */
     boolean indentMultiline();
 }
