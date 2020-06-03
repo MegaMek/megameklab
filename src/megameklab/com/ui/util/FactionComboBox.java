93c71a38c5b4967c9ee8422d3acad0086f108075
Carl Spain <neoancient@megamek.org>
2020-06-03 12:09:32 -0500

    Make tech base options dependent on the selected faction rather than the other way around.
    This allows a distinction between IS units built by IS factions and those built by Clan factions (which allow mixed tech much earlier).

--- src/megameklab/com/ui/util/FactionComboBox.java
+++ src/megameklab/com/ui/util/FactionComboBox.java
@@ -1,13 +1,19 @@
-/**
- * 
+/*
+ * MegaMekLab - Copyright (C) 2020 - The MegaMek Team
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
  */
 package megameklab.com.ui.util;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import megamek.client.ratgenerator.FactionRecord;
 import megamek.client.ratgenerator.RATGenerator;
@@ -28,30 +34,30 @@ public class FactionComboBox extends CustomComboBox<Integer> {
      */
     private static final long serialVersionUID = 4722914142736815170L;
     
-    private Map<Integer,String> displayNames = new HashMap<>();
+    private final Map<Integer,String> displayNames = new HashMap<>();
     
     public FactionComboBox() {
         super();
-        setRenderer(new Renderer<Integer>(i -> displayNames.get(i)));
+        setRenderer(new Renderer<>(displayNames::get));
         while (!RATGenerator.getInstance().isInitialized()) {
             try {
                 Thread.sleep(50);
-            } catch (InterruptedException e) {
+            } catch (InterruptedException ignored) {
             }
         }
     }
     
-    public void refresh(int year, boolean clan) {
+    public void refresh(int year) {
         displayNames.clear();
         for (int i = 0; i < ITechnology.MM_FACTION_CODES.length; i++) {
             final FactionRecord fRec = RATGenerator.getInstance().getFaction(ITechnology.MM_FACTION_CODES[i]);
             // TA will generate a null value because the RAT Generator doesn't distinguish between TH and TA.
-            if ((null != fRec) && (fRec.isClan() == clan) && (fRec.isActiveInYear(year))) {
+            if ((null != fRec) && (fRec.isActiveInYear(year))) {
                 displayNames.put(i, fRec.getName(year));
             }
         }
         List<Integer> sorted = new ArrayList<>(displayNames.keySet());
-        Collections.sort(sorted, (i1, i2) -> displayNames.get(i1).compareTo(displayNames.get(i2)));
+        sorted.sort(Comparator.comparing(displayNames::get));
         removeAllItems();
         addItem(-1);
         displayNames.put(-1, "Any");
