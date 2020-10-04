9b1cf4af132d608b120ae2b8b9df0a5bfcb3c475
HammerGS <drivingguy@hotmail.com>
2020-10-04 16:47:35 -0600

    Ready to Develop <v0.47.12>

--- src/megameklab/com/MegaMekLab.java
+++ src/megameklab/com/MegaMekLab.java
@@ -49,7 +49,7 @@ import megameklab.com.util.CConfig;
 import megameklab.com.util.UnitUtil;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.47.11";
+    public static final String VERSION = "0.47.12-SNAPSHOT";
 
     private static MMLogger logger = null;
 
