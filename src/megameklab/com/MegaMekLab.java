3f140d53918ea43a7e9439ad5edc3a2efed7f9dc
HammerGS <drivingguy@hotmail.com>
2020-11-08 15:38:10 -0700

    Development snapshot <v0.47.12>

--- src/megameklab/com/MegaMekLab.java
+++ src/megameklab/com/MegaMekLab.java
@@ -49,7 +49,7 @@ import megameklab.com.util.CConfig;
 import megameklab.com.util.UnitUtil;
 
 public class MegaMekLab {
-    public static final String VERSION = "0.47.12-SNAPSHOT";
+    public static final String VERSION = "0.47.12";
 
     private static MMLogger logger = null;
 
