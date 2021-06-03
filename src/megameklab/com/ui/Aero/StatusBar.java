f232017f128141882a94edf450cc33bd8da09fe8
Carl Spain <neoancient@megamek.org>
2021-06-03 11:15:12 -0500

    Show unused tonnage in the status bar.

--- src/megameklab/com/ui/Aero/StatusBar.java
+++ src/megameklab/com/ui/Aero/StatusBar.java
@@ -118,7 +118,7 @@ public class StatusBar extends ITab {
         }
         heatSink.setVisible(getAero().getEntityType() == Entity.ETYPE_AERO);
 
-        tons.setText("Tonnage: " + currentTonnage + "/" + tonnage);
+        tons.setText(String.format("Tonnage: %.1f/%.1f (%.1f Remaining)", currentTonnage, tonnage, tonnage - currentTonnage));
         tons.setToolTipText("Current Tonnage/Max Tonnage");
         if (currentTonnage > tonnage) {
             tons.setForeground(Color.red);
