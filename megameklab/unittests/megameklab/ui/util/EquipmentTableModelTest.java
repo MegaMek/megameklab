4017b02c03049b9443a7fda4eab8534275b7b095
sleet01 <sleet01@hotmail.com>
2024-02-18 04:04:13 -0800

    After refresh of gradle dependencies

--- megameklab/unittests/megameklab/ui/util/EquipmentTableModelTest.java
+++ megameklab/unittests/megameklab/ui/util/EquipmentTableModelTest.java
@@ -2,26 +2,28 @@ package megameklab.ui.util;
 
 import megamek.common.EquipmentType;
 import megamek.common.ITechManager;
+import megameklab.ui.generalUnit.BasicInfoView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
-import org.mockito.*;
+import org.mockito.Mockito.*;
+
+import static org.mockito.Mockito.mock;
 
 class EquipmentTableModelTest {
 
     private ArrayList<EquipmentType> data = new ArrayList<>();
-    private ITechManager techManager;
+    private BasicInfoView techManager;
 
     @BeforeEach
     void setUp() {
-        techManager = null;
+        techManager = mock(BasicInfoView.class);
     }
 
     @Test
     void testGetValueAt_null_weapon() {
-
 
     }
 }
