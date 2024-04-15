a96a6a1a3961a6389cbe28e3f3fec317d0e172c8
sleet01 <sleet01@hotmail.com>
2024-04-15 00:15:57 -0700

    Fix #5252: critical assignment tab eq list missing keywords like "Ammo" and "[Half]" or "[Full]"

--- megameklab/src/megameklab/util/UnitUtil.java
+++ megameklab/src/megameklab/util/UnitUtil.java
@@ -1143,7 +1143,8 @@ public class UnitUtil {
      */
     public static String getCritName(Entity unit, EquipmentType eq) {
         String name = eq.getName();
-        if (name.length() > 22) {
+        // Only shorten non-ammo; getShortName leaves off "Ammo" and "[Half]" that we want
+        if (name.length() > 22 && !(eq instanceof AmmoType) ) {
             name = eq.getShortName();
         }
         if (unit.isMixedTech()
