e5a13f241d0b1d651443ce812943afa2f9a9c1bd
Simon <juliez@outlook.de>
2024-06-17 18:40:48 +0200

    mass print RS

--- megameklab/src/megameklab/printing/CGLMassPrinter.java
+++ megameklab/src/megameklab/printing/CGLMassPrinter.java
@@ -4,62 +4,56 @@ import megamek.common.*;
 import megameklab.MMLOptions;
 import megameklab.util.CConfig;
 import megameklab.util.UnitPrintManager;
-import megameklab.util.UnitUtil;
-import org.apache.logging.log4j.LogManager;
 
-import javax.swing.*;
 import java.io.File;
-import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
-import static java.lang.System.exit;
-import static megameklab.util.UnitPrintManager.getExportFile;
-
 public class CGLMassPrinter {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        File sheetsDir = new File("sheets");
+        if (!sheetsDir.exists() || !sheetsDir.isDirectory()) {
+            System.err.println("Error: sheets directory does not exist or is not a directory");
+            System.exit(1);
+        }
         Locale.setDefault(new MMLOptions().getLocale());
         EquipmentType.initializeTypes();
         CConfig.load();
         MechSummaryCache cache = MechSummaryCache.getInstance(true);
-//        try {
-//            Entity entity = new MechFileParser(new File("data/mechfiles/mechs/3039u/Atlas AS7-D.mtf")).getEntity();
-//            if (entity != null) {
-//                File file = new File("sheets", "atlas.pdf");
-//                try {
-//                    UnitPrintManager.exportUnits(Collections.singletonList(entity), file, true);
-//                    System.out.println("printed ");
-//                } catch (Exception e) {
-//                    System.out.println("print error ");
-//                }
-//            } else {
-//                System.out.println("null entity ");
-//            }
-//        } catch (Exception ex) {
-//            LogManager.getLogger().error("", ex);
-//        }
 
-        int i = 0;
         for (MechSummary ms : cache.getAllMechs()) {
             if (!ms.isCanon()) {
                 continue;
             }
-            Entity entity = ms.loadEntity();
-            if (entity != null) {
-                File file = new File("sheets", sanitize(ms.getName()) + ".pdf");
-                try {
-                    UnitPrintManager.exportUnits(Collections.singletonList(entity), file, true);
-                    System.out.println("printed " + file);
-                    i++;
-                } catch (Exception e) {
-                    System.out.println("print error " + ms.getName());
-                }
-            } else {
-                System.out.println("null entity " + ms.getName());
+
+
+            // 1 - uncomment this block and cycle all the start characters A-Z (only uppercase)
+            if (!ms.getName().toUpperCase().startsWith("Y")) {
+                continue;
             }
-            if (i>0) exit(0);
+
+            // 2 - uncomment this block, comment the above block, run once more
+//            if (ms.getName().toUpperCase().charAt(0) <= 'Z' && ms.getName().toUpperCase().charAt(0) >= 'A') {
+//                continue;
+//            }
+
+            Entity entity = ms.loadEntity();
+            if (entity != null && !(entity instanceof GunEmplacement)) {
+                File file = new File("sheets", ms.getMulId() + "_"
+                        + sanitize(ms.getChassis()) + "_"
+                        + sanitize(ms.getModel()) + ".pdf");
+                try {
+                    UnitPrintManager.exportUnits(List.of(entity), file, true);
+                    System.out.println("printed " + file);
+                } catch (Exception e) {
+                    System.out.println("print error ");
+                }
+                Thread.sleep(2000);
+            }
         }
+
+        Thread.sleep(10000);
     }
 
     private static String sanitize(String original) {
