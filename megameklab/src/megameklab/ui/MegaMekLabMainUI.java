b25dde65751705b84609a3657fd24508ae75d45f
Exeea <dev@exeea.com>
2025-03-25 23:08:11 +0100

    fixed tab title

--- megameklab/src/megameklab/ui/MegaMekLabMainUI.java
+++ megameklab/src/megameklab/ui/MegaMekLabMainUI.java
@@ -156,10 +156,10 @@ public abstract class MegaMekLabMainUI extends JFrame
         Entity entity = getEntity();
         setTitle(entity.getFullChassis() + " " + entity.getModel() + fileInfo);
         if (configPane.hasDetachedTabs()) {
-            configPane.setDetachedTabsPrefixTitle(entity.getDisplayName());
+            configPane.setDetachedTabsPrefixTitle(entity.getShortNameRaw());
         }
         if (owner != null) {
-            owner.setTabName(entity.getDisplayName(), this);
+            owner.setTabName(entity.getShortNameRaw(), this);
         }
     }
 
