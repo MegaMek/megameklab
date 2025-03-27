aa527b32cb89baf0b62a81ab7f8854b1fdb8cf38
Exeea <dev@exeea.com>
2025-03-27 04:12:29 +0100

    weakreference for dockableInstances

--- megameklab/src/megameklab/ui/util/EnhancedTabbedPane.java
+++ megameklab/src/megameklab/ui/util/EnhancedTabbedPane.java
@@ -130,7 +130,7 @@ public class EnhancedTabbedPane extends JTabbedPane {
     private int previewTabIndex = -1;
     private static final String PREVIEW_TAB_ID = "PREVIEW_TAB";
     private String dockGroupId;
-    private static final List<EnhancedTabbedPane> dockableInstances = new CopyOnWriteArrayList<>();
+    private static final List<WeakReference<EnhancedTabbedPane>> dockableInstances = new CopyOnWriteArrayList<>();
 
     private static class DragState {
         int tabIndex = -1;
@@ -804,7 +804,9 @@ public class EnhancedTabbedPane extends JTabbedPane {
         if (dockGroupId == null) {
             return null;
         }
-        for (EnhancedTabbedPane pane : dockableInstances) {
+        dockableInstances.removeIf(ref -> ref.get() == null);
+        for (WeakReference<EnhancedTabbedPane> ref : dockableInstances) {
+            final EnhancedTabbedPane pane = ref.get();
             if (pane != this && pane.dockGroupId != null && pane.dockGroupId.equals(this.dockGroupId)
                     && pane.isShowing()) {
                 try {
@@ -1790,13 +1792,13 @@ public class EnhancedTabbedPane extends JTabbedPane {
     public void addNotify() {
         super.addNotify();
         if (dockGroupId != null) {
-            dockableInstances.add(this);
+            dockableInstances.add(new WeakReference<>(this));
         }
     }
 
     @Override
     public void removeNotify() {
-        dockableInstances.remove(this);
+        dockableInstances.removeIf(ref -> ref.get() == this || ref.get() == null);
         if (isShowingDockingPreview) {
             removePreviewTab();
         }
