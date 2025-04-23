64f4e0dc40ca6427ffd099b6682051e252c54a36
Exeea <dev@exeea.com>
2025-04-23 21:58:17 +0200

    prompts

--- megameklab/src/megameklab/ui/MegaMekLabTabbedUI.java
+++ megameklab/src/megameklab/ui/MegaMekLabTabbedUI.java
@@ -159,7 +159,8 @@ public class MegaMekLabTabbedUI extends JFrame implements MenuBarOwner, ChangeLi
             public void onTabRemoved(int tabIndex, Component component) {
                 // If you try to close the last tab, we close this window
                 if (tabs.getTabCount() < 1) {
-                    if (openWindows.size() == 1 && (CConfig.getBooleanParam(CConfig.MISC_APPLICATION_EXIT_PROMPT))
+                    final boolean willTerminate = (openWindows.size() == 1) && (!ForceBuildUI.hasInstance() || !ForceBuildUI.getInstance().isShowing());
+                    if (willTerminate && (CConfig.getBooleanParam(CConfig.MISC_APPLICATION_EXIT_PROMPT))
                             && !noTabsOpenExitPrompt()) {
                         newTab();
                     } else {
