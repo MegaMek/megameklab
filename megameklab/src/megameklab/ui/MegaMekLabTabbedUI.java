2e826ee4a508c0ae14e75a559ea5cb128e961e81
Exeea <dev@exeea.com>
2025-04-23 11:56:23 +0200

    closing splash before opening editor

--- megameklab/src/megameklab/ui/MegaMekLabTabbedUI.java
+++ megameklab/src/megameklab/ui/MegaMekLabTabbedUI.java
@@ -433,6 +433,11 @@ public class MegaMekLabTabbedUI extends JFrame implements MenuBarOwner, ChangeLi
             targetWindow.setVisible(true);
         }
 
+        if (StartupGUI.hasInstance()) {
+            StartupGUI.getInstance().getFrame().setVisible(false);
+            StartupGUI.getInstance().getFrame().dispose();
+        }
+        
         UnitUtil.updateLoadedUnit(entityToFind);
         MegaMekLabMainUI newUi = UiLoader.getUI(entityToFind, "");
         targetWindow.addTab(newUi, true);
@@ -440,10 +445,6 @@ public class MegaMekLabTabbedUI extends JFrame implements MenuBarOwner, ChangeLi
         targetWindow.setVisible(true);
         targetWindow.toFront();
         targetWindow.requestFocus();
-        if (StartupGUI.hasInstance()) {
-            StartupGUI.getInstance().getFrame().setVisible(false);
-            StartupGUI.getInstance().getFrame().dispose();
-        }
     }
 
     /**
