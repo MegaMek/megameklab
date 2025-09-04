84ea6d92135552dc1a48cb2fa702022ea50791f8
Exeea <dev@exeea.com>
2025-09-04 23:08:31 +0200

    text fix

--- megameklab/src/megameklab/printing/InventoryWriter.java
+++ megameklab/src/megameklab/printing/InventoryWriter.java
@@ -397,9 +397,9 @@ public class InventoryWriter {
         }
         if (sheet.getEntity() instanceof Mek mek && mek.hasFullHeadEject()) {
             var mounted = new MiscMounted(sheet.getEntity(), new MiscType() {{
-                name = "Full Head Ejection SystemFluff";
-                shortName = "Full Head Eject SystemFluff";
-                internalName = "Full Head Ejection SystemFluff";
+                name = "Full Head Ejection System";
+                shortName = "Full Head Eject System";
+                internalName = "Full Head Ejection System";
             }});
             mounted.setLocation(Mek.LOC_NONE);
             equipment.add(new StandardInventoryEntry(mounted));
