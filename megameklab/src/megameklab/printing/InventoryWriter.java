6a5cd11ab7d3305ad71b4e0e0882b2170302f80b
exeea <48771891+exeea@users.noreply.github.com>
2026-06-22 18:39:24 +0200

    fixed HEAT column overlapping LOC in aero units

--- megameklab/src/megameklab/printing/InventoryWriter.java
+++ megameklab/src/megameklab/printing/InventoryWriter.java
@@ -106,7 +106,7 @@ public class InventoryWriter {
         BAY("Bay", 0.02),
         LOCATION("Loc", 0.40, 0.49),
         LOCATION_NO_HEAT("Loc", 0.45, 0.54),
-        HEAT("Ht", 0.465, 0.57),
+        HEAT("Ht", 0.465, 0.6),
         DAMAGE("Dmg", 0.5, 0.5, 0.5),
         MOD("Tn", 0.725, 0.625, 0.65),
         MIN("Min", 0.785, 0.785, 0.72),
