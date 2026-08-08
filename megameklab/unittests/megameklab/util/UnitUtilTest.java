3254889d9a617ba27c12a8cc33f2851ebab45936
exeea <dev@exeea.com>
2026-08-08 16:51:37 +0200

    .

--- megameklab/unittests/megameklab/util/UnitUtilTest.java
+++ megameklab/unittests/megameklab/util/UnitUtilTest.java
@@ -33,8 +33,8 @@
 
 package megameklab.util;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
